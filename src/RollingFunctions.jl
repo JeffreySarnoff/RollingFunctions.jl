fc67f8f6643eeb88330beab7229cf35ab325e49d
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2017-03-29 00:08:52 -0400

    Update RollingFunctions.jl

--- src/RollingFunctions.jl
+++ src/RollingFunctions.jl
@@ -2,6 +2,9 @@ module RollingFunctions.jl
 
 export rolling, rolling_ahead, rolling_behind, rolling_first, rolling_final
 
+const Rollable = AbstractVector{T} where T
+const Runnable = AbstractArray{T}  where T
+
 include("rolling.jl")
 include("running.jl")
 
