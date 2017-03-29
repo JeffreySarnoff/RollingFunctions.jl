bdb7af7dd0e14269f398fe17fa6049d372312ed6
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2017-03-29 01:52:56 -0400

    Update runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,8 +1,14 @@
 using RollingFunctions
 using Base.Test
 
-idata = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43]
-fdata = [1.0, 1.0, 2.0, 3.0, 5.0, 8.0, 13.0, 21.0, 34.0, 55.0]
+fdata = [1.0, 3.0, 5.0, 8.0,  21.0, 34.0]
 
-@test rolling(mean, 4, fdata) == [1.0,  1.0,  2.0, 1.75, 2.75, 4.5, 7.25, 11.75, 19.0, 30.75]
-@test rolling(median, 6, idata) ==  [2, 3, 5, 7, 11, 6, 9, 12, 15, 18, 21, 26, 30, 34]
+@test rolling(mean, 4, fdata)       == [1.00, 3.00,  5.0,  4.25,  9.25, 17.00]
+@test rolling_first(mean, 4, fdata) == [4.25, 9.25, 17.0,  8.00, 21.00, 34.00] 
+@test rolling_final(mean, 4, fdata) == [1.00, 3.00,  5.0,  4.25,  9.25, 17.00]
+@test rolling_start(mean, 4, fdata) == [4.25, 9.25, 17.0, 21.00, 27.50, 34.00]
+@test rolling_start(mean, 4, fdata) == [1.00, 2.00,  3.0,  4.25,  9.25, 17.00]
+
+
+
+
