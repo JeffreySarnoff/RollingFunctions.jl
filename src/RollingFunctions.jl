6b61a18c36fad9c0359b71048b5f05ca5be21bb5
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2017-03-29 00:21:03 -0400

    Update RollingFunctions.jl

--- src/RollingFunctions.jl
+++ src/RollingFunctions.jl
@@ -1,9 +1,20 @@
 module RollingFunctions.jl
 
-export rolling, rolling_ahead, rolling_behind, rolling_first, rolling_final
+export RollControl, rolling, rolling_ahead, rolling_behind, rolling_first, rolling_final
 
 const Rollable = AbstractVector{T} where T
 const Runnable = AbstractArray{T}  where T
+
+struct RollControl
+    keep_length::Bool
+    taiper_ends::Bool
+    look_ahead::Bool      #  ahead && around is around (even n extra obs after midpoint)
+    look_around::Bool     # !ahead && around is around (even n extra obsbefore midpoint)
+end
+
+RollControl() = RollControl(true, false, true, false)
+RollControl(tf::Bool) = RollControl(tf, tf, true, false)
+
 
 include("rolling.jl")
 include("running.jl")
