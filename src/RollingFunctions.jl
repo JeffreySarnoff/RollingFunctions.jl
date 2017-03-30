f5797bbe1975d717797112715508334c2f5e660a
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2017-03-30 02:50:33 -0400

    Update RollingFunctions.jl

--- src/RollingFunctions.jl
+++ src/RollingFunctions.jl
@@ -1,9 +1,8 @@
 module RollingFunctions
 
 export running_minimum, running_maximum, running_span,
-       running_median, running_mode, 
-       running_mean, running_geomean, running_harmmean,
-       running_variation, running_sem, runnning_mad, 
+       running_median, running_mode, running_mean, running_geomean, running_harmmean,
+       running_variation, running_sem, runnning_mad, running_entropy,
        AbstractRoller, Roller, Runner, 
        FullSpansRoller, FirstSpansRoller, FinalSpansRoller
 
