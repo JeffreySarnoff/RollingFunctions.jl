75f8a21b87c091e5944869ec8374c381bfbb208a
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2017-03-30 02:55:18 -0400

    Update running.jl

--- src/running.jl
+++ src/running.jl
@@ -1,5 +1,14 @@
-running_minimum(n, data) = Runner(FinalSpansRoller(minimum,n), data)
-running_maximum(n, data) = Runner(FinalSpansRoller(maximum,n), data)
+running_minimum(n, data)  = Runner(FinalSpansRoller(minimum,n), data)
+running_maximum(n, data)  = Runner(FinalSpansRoller(maximum,n), data)
+running_span(n, data)     = Runner(FinalSpansRoller(span,n), data)
 
-running_median(n, data)  = Runner(FinalSpansRoller(median,n), data)
-running_mean(n, data)    = Runner(FinalSpansRoller(mean,n), data)
+running_median(n, data)   = Runner(FinalSpansRoller(median,n), data)
+running_mode(n, data)     = Runner(FinalSpansRoller(mode,n), data)
+running_mean(n, data)     = Runner(FinalSpansRoller(mean,n), data)
+running_geomean(n, data)  = Runner(FinalSpansRoller(geomean,n), data)
+running_harmmean(n, data) = Runner(FinalSpansRoller(harmmean,n), data)
+
+running_sem(n, data)      = Runner(FinalSpansRoller(sem,n), data)
+running_mad(n, data)      = Runner(FinalSpansRoller(mad,n), data)
+running_variation(n, data)= Runner(FinalSpansRoller(variation,n), data)
+running_entropy(n, data)  = Runner(FinalSpansRoller(entropy,n), data)
