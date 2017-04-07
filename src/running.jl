7d923265e8beb03556de0f08a7146cf9cdda707a
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2017-04-07 01:57:53 -0400

    Update running.jl

--- src/running.jl
+++ src/running.jl
@@ -1,11 +1,11 @@
-runmin(n, data)  = runner(Roller(rolling, minimum, n), data)
-runmax(n, data)  = runner(Roller(rolling, maximum, n), data)
 runspan(n, data) = runner(Roller(rolling, span, n), data)
-
 runstd(n, data)  = runner(Roller(rolling, std, n), data)
 runvar(n, data)  = runner(Roller(rolling, var, n), data)
 runmad(n, data)  = runner(Roller(rolling, mad, n), data)
 runsem(n, data)  = runner(Roller(rolling, sem, n), data)
+
+runminimum(n, data)   = runner(Roller(rolling, minimum, n), data)
+runmaximum(n, data)   = runner(Roller(rolling, maximum, n), data)
 
 runmedian(n, data)    = runner(Roller(rolling, median, n), data)
 runmode(n, data)      = runner(Roller(rolling, mode, n), data)
@@ -17,15 +17,14 @@ runvariation(n, data) = runner(Roller(rolling, variation, n), data)
 runentropy(n, data)   = runner(Roller(rolling, entropy, n), data)
 
 
-runmin_backfill(n, data)  = runner(Roller(rolling_backfill, minimum, n), data)
-runmax_backfill(n, data)  = runner(Roller(rolling_backfill, maximum, n), data)
 runspan_backfill(n, data) = runner(Roller(rolling_backfill, span, n), data)
-
 runstd_backfill(n, data)  = runner(Roller(rolling_backfill, std, n), data)
 runvar_backfill(n, data)  = runner(Roller(rolling_backfill, var, n), data)
 runmad_backfill(n, data)  = runner(Roller(rolling_backfill, mad, n), data)
 runsem_backfill(n, data)  = runner(Roller(rolling_backfill, sem, n), data)
 
+runminimum_backfill(n, data)   = runner(Roller(rolling_backfill, minimum, n), data)
+runmaximum_backfill(n, data)   = runner(Roller(rolling_backfill, maximum, n), data)
 runmedian_backfill(n, data)    = runner(Roller(rolling_backfill, median, n), data)
 runmode_backfill(n, data)      = runner(Roller(rolling_backfill, mode, n), data)
 runmean_backfill(n, data)      = runner(Roller(rolling_backfill, mean, n), data)
