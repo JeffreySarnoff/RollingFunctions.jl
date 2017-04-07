2175d5b12b07dd1d2ca83689a9bd4b886217963c
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2017-04-07 00:53:37 -0400

    Update rolling.jl

--- src/rolling.jl
+++ src/rolling.jl
@@ -8,16 +8,61 @@ length(result) == length(data) - span + 1
 function rolling{T}(fn::Function, span::Int, data::Vector{T})
     n_in  = length(data)
     n_out = n_in - span + 1
-    res = zeros(T, n_in)
+    res = zeros(T, n_out)
     span -= 1 
     
-    res[1:span] = data[1:span]
     for i in 1:n_out
-        res[i+span] = fn(data[i:i+span])
+        res[i] = fn(data[i:i+span])
     end    
     
     return res
 end
+
+"""
+rolling_locf(fn, span, data) is is rolling last_obs_carry_forward      
+
+applies fn to successive sub-spans of data    
+carries the (end-span)_th result forward
+
+length(result) == length(data)
+"""
+function rolling_locf{T}(fn::Function, span::Int, data::Vector{T})
+    n_in  = length(data)
+    n_out = n_in - span + 1   
+    res   = zeros(T, n_in)
+    res[1:n_out] = rolling(fn, span, data)
+    res[n_out+1:end] = res[n_out]
+   return res
+end
+
+"""
+rolling_focb(fn, span, data) is rolling first_obs_carry_backward    
+
+applies fn to successive sub-spans of data    
+carries the span_th result backward
+
+length(result) == length(data)
+"""
+function rolling_focb{T}(fn::Function, span::Int, data::Vector{T})
+    n_in  = length(data)
+    n_out = n_in - span   
+    res   = zeros(T, n_in)
+    res[span:n_out+span] = rolling(fn, span, data)
+    res[1:span-1] = res[span]
+   return res
+end
+
+"""
+rolling_boca(fn, span, data) is rolling bounding_obs_carry_around   
+
+applies fn to successive sub-spans of data    
+averages rolling_locf and rolling_focb
+
+length(result) == length(data)
+"""
+function rolling_boca{T}(fn::Function, span::Int, data::Vector{T})
+    return 0.5*rolling_locf(fn, span, data) + 0.4*rolling_focb(fn, span, data)
+end    
 
 """
 rolling_finish(fn, span, data)    
