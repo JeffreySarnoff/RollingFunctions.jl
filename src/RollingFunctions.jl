270e2dad4b240a40e6dbe48b03d50fc6253ec4c1
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2017-04-06 23:38:00 -0400

    Update RollingFunctions.jl

--- src/RollingFunctions.jl
+++ src/RollingFunctions.jl
@@ -15,26 +15,24 @@ struct Roller{T} <: AbstractRoller{T}
     span::Int64
 end
 
-const FullSpansRoller  = Roller{Val{:full}}    # use only completely spanned values (shorter result)
-const FirstSpansRoller = Roller{Val{:first}}   # final values are spanned coarsely  (equilength result, tapering at end)
-const FinalSpansRoller = Roller{Val{:final}}   # first values are spanned coarsely  (equilength result, tapering at start)
+const FullSpansRoller   = Roller{Val{:full}}     # use only completely spanned values (shorter result)
+const StartSpansRoller  = Roller{Val{:start}}    # final values are spanned coarsely  (equilength result, tapering at end)
+const FinishSpansRoller = Roller{Val{:finish}}   # first values are spanned coarsely  (equilength result, tapering at start)
+const FirstSpansRoller  = Roller{Val{:first}}    # final values are filled forward   (equilength result, carrying forward at end)
+const FinalSpansRoller  = Roller{Val{:final}}    # first values are filled backward   (equilength result, carrying backward at start)
 
 
 struct Runner{T, R} <: AbstractRoller{T}
     roll::Roller{T}
 end
 
-function Runner{R}(roll::FullSpansRoller, data::Vector{R})
-    rolling(roll.fn, roll.span, data)
-end
-
-function Runner{R}(roll::FirstSpansRoller, data::Vector{R})
-    rolling_start(roll.fn, roll.span, data)
-end
-
-function Runner{R}(roll::FinalSpansRoller, data::Vector{R})
-    rolling_finish(roll.fn, roll.span, data)
-end
+for T in (:FullSpansRoller, :StartSpansRoller, :FinishSpansRoller, :FirstSpansRoller, :FinalSpansRoller)
+    @eval begin
+       function Runner{R}(roll::$T, data::Vector{R})
+           rolling(roll.fn, roll.span, data)
+       end
+    end
+end       
 
 
 include("rolling.jl")
