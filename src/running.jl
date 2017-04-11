967732c50c8177ac224fde3aae844d0065dd64cf
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2017-04-11 12:25:09 -0400

    Update running.jl

--- src/running.jl
+++ src/running.jl
@@ -3,8 +3,8 @@ for (F,R) in [(:minimum, :rollminimum), (:maximum, :rollmaximum),
               (:std, :rollstd), (:var, :rollvar), (:mad, :rollmad)]
     @eval begin
         $R{T<:Number}(n::Int, data::Vector{T}) = runner(Roller(rolling, $F), n, data)
-        $R{T<:Number}(::Type{AtFirst}, n::Int, data::Vector{T})  = runner(Roller(rolling_fill_first, $F), n, data)
-        $R{T<:Number}(::Type{AtLast}, n::Int, data::Vector{T})   = runner(Roller(rolling_fill_last, $F), n, data)
-        $R{T<:Number}(::Type{AtCenter}, n::Int, data::Vector{T}) = runner(Roller(rolling_fill_center, $F), n, data)
+        $R{T<:Number}(::Type{FillFirst}, n::Int, data::Vector{T})  = runner(Roller(rolling_fill_first, $F), n, data)
+        $R{T<:Number}(::Type{FillLast}, n::Int, data::Vector{T})   = runner(Roller(rolling_fill_last, $F), n, data)
+        $R{T<:Number}(::Type{FillCentered}, n::Int, data::Vector{T}) = runner(Roller(rolling_fill_centered, $F), n, data)
     end
 end
