769579b022b37e2c776a23a74670093c591eb3c8
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2017-04-11 12:26:48 -0400

    Update running.jl

--- src/running.jl
+++ src/running.jl
@@ -5,6 +5,6 @@ for (F,R) in [(:minimum, :rollminimum), (:maximum, :rollmaximum),
         $R{T<:Number}(n::Int, data::Vector{T}) = runner(Roller(rolling, $F), n, data)
         $R{T<:Number}(::Type{FillFirst}, n::Int, data::Vector{T})  = runner(Roller(rolling_fill_first, $F), n, data)
         $R{T<:Number}(::Type{FillLast}, n::Int, data::Vector{T})   = runner(Roller(rolling_fill_last, $F), n, data)
-        $R{T<:Number}(::Type{FillCentered}, n::Int, data::Vector{T}) = runner(Roller(rolling_fill_centered, $F), n, data)
+        $R{T<:Number}(::Type{FillCenter}, n::Int, data::Vector{T}) = runner(Roller(rolling_fill_center, $F), n, data)
     end
 end
