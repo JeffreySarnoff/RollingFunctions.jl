e4e9ab9994504316873833eb37fe1f4b87d753e9
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2017-04-12 02:24:30 -0400

    Update RollingFunctions.jl

--- src/RollingFunctions.jl
+++ src/RollingFunctions.jl
@@ -56,28 +56,11 @@ end
 function roll{T}(roller::Roller{FillFirstPart, FillWithRepeated}, data::Vector{T})
     return rolling_fill_first(roller.rolling, roller.rollspan, data)
 end
-
-#=
-function roll{T}(roller::Roller{FillFirstPart, FillWithRepeated}, data::Vector{T})
-    return rolling_fill_first(roller.rolling, roller.rollspan, data)
+function roll{T}(roller::Roller{FillLastPart, FillWithRepeated}, data::Vector{T})
+    return rolling_fill_last(roller.rolling, roller.rollspan, data)
 end
-
-r2=Roller(FirstPart,WithRepeated,mean,5)
-data=map(Float64,collect(1:8:100))
-result=roll(r2,data)
-=#
-
-function roll{FW,T}(roller::Roller{NoPart,FW}, data::Vector{T})
-       return rolling(roller.rolling, roller.span, data)
-end
-function roll{FP,T}(roller::Roller{FP, WithNothing}, data::Vector{T})
-       return rolling(roller.rolling, roller.span, data)
-end
-function roll{T}(roller::Roller{FirstPart, WithRepeated}, data::Vector{T})
-       return rolling_fill_first(roller.rolling, roller.span, data)
-end
-function roll{T}(roller::Roller{LastPart, WithRepeated}, data::Vector{T})
-       return rolling_fill_last(roller.rolling, roller.span, data)
+function roll{T}(roller::Roller{FillBothParts, FillWithRepeated}, data::Vector{T})
+    return rolling_fill_both(roller.rolling, roller.rollspan, data)
 end
 
 
