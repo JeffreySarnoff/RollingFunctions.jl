ac4aaff52a16b503391532d6d6c96768f6740170
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2017-04-12 02:26:08 -0400

    Update running.jl

--- src/running.jl
+++ src/running.jl
@@ -1,10 +1,10 @@
-for (F,R) in [(:minimum, :rollminimum), (:maximum, :rollmaximum),
+for (F,N) in [(:minimum, :rollminimum), (:maximum, :rollmaximum),
               (:median, :rollmedian), (:mode, :rollmode), (:mean, :rollmean),
               (:std, :rollstd), (:var, :rollvar), (:mad, :rollmad)]
     @eval begin
-        $R{T<:Number}(n::Int, data::Vector{T}) = runner(Roller(rolling, $F), n, data)
-        $R{T<:Number}(::Type{FillFirst}, n::Int, data::Vector{T})  = runner(Roller(rolling_fill_first, $F), n, data)
-        $R{T<:Number}(::Type{FillLast}, n::Int, data::Vector{T})   = runner(Roller(rolling_fill_last, $F), n, data)
-        $R{T<:Number}(::Type{FillCenter}, n::Int, data::Vector{T}) = runner(Roller(rolling_fill_center, $F), n, data)
+        $N{T<:Number}(n::Int, data::Vector{T}) = runner(Roller(rolling, $F), n, data)
+        $N{T<:Number}(::Type{FillFirst}, n::Int, data::Vector{T})  = runner(Roller(rolling_fill_first, $F), n, data)
+        $N{T<:Number}(::Type{FillLast}, n::Int, data::Vector{T})   = runner(Roller(rolling_fill_last, $F), n, data)
+        $N{T<:Number}(::Type{FillBoth}, n::Int, data::Vector{T}) = runner(Roller(rolling_fill_center, $F), n, data)
     end
 end
