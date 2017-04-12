f103f928e614823bb20fbc471edc3baf82071660
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2017-04-12 03:37:11 -0400

    Update running.jl

--- src/running.jl
+++ src/running.jl
@@ -1,3 +1,20 @@
+
+rollmean{FW, T<:Number}(::Type{FillFirstPart}, ::Type{FW}, rollspan::Int, data::Vector{T}) =
+    Roller(FirstPart, WithRepeated, mean, rollspan)
+rollmean{FW, T<:Number}(::Type{FillLastPart}, ::Type{FW}, rollspan::Int, data::Vector{T}) =
+    Roller(LastPart, WithRepeated, mean, rollspan)
+
+#=
+function roll{T}(roller::Roller{FillFirstPart, FillWithRepeated}, data::Vector{T})
+    return rolling_fill_first(roller.rolling, roller.rollspan, data)
+end
+
+rolling_mean_fill_first=Roller(FirstPart,WithRepeated,mean,5)
+
+data = map(Float64,collect(1:8:100))
+result = roll(rolling_mean_fill_first, data)
+=#
+
 for (F,N) in [(:minimum, :rollminimum), (:maximum, :rollmaximum),
               (:median, :rollmedian), (:mode, :rollmode), (:mean, :rollmean),
               (:std, :rollstd), (:var, :rollvar), (:mad, :rollmad)]
