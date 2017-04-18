350ba63043e59e4a624eac9390f802f9da011689
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2017-04-18 08:53:16 -0400

    Update runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -8,8 +8,8 @@ fdata = [1.0, 3.0, 5.0, 8.0,  6.0, 9.0]
 
 @test rolling_fill_first(mean, 4, fdata) == [4.25, 4.25, 4.25, 4.25, 5.5, 7.0]
 @test rolling_fill_last(mean, 4, fdata) == [4.25, 5.5, 7.0, 7.0, 7.0, 7.0]
-@test string(rolling_fill_first(mean, 4, fdata, NaN)) == string([NaN, NaN, NaN, 4.25, 5.5, 7.0])
-@test string(rolling_fill_last(mean, 4, fdata, NaN)) == string([4.25, 5.5, 7.0, NaN, NaN, NaN])
+@test string(rolling_fill_first(mean, 4, NaN, fdata)) == string([NaN, NaN, NaN, 4.25, 5.5, 7.0])
+@test string(rolling_fill_last(mean, 4, NaN, fdata)) == string([4.25, 5.5, 7.0, NaN, NaN, NaN])
 
 @test rolling_taper_first(mean, 4, 3, fdata) == [3.0, 3.0, 3.0, 4.25, 5.5, 7.0]
 @test rolling_taper_first(mean, 4, 2, fdata) == [2.0, 2.0, 3.0, 4.25, 5.5, 7.0]
