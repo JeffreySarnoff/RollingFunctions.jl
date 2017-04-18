348a116d9d5269e6b643b87a3ac60c7949ce39b6
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2017-04-18 09:34:51 -0400

    Update RollingFunctions.jl

--- src/RollingFunctions.jl
+++ src/RollingFunctions.jl
@@ -4,8 +4,7 @@ export roll_minimum, roll_maximum, roll_mean,
        roll_std, roll_var, roll_mad,
        roll_minimum_filled, roll_maximum_filled, roll_mean_filled, 
        roll_std_filled, roll_var_filled, roll_mad_filled,
-       roll_minimum_tapered, roll_maximum_tapered, roll_mean_tapered.
-       roll_std_tapered, roll_var_tapered, roll_mad_tapered,
+       roll_minimum_tapered, roll_maximum_tapered, roll_mean_tapered
        # these are used to make other roll_ functions
        rolling, rolling_fill_first, rolling_fill_last, rolling_taper_first, rolling_taper_last
        
