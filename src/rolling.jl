15aeae9c03ebde79abc3d2f2d6decbb130b928ba
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2017-04-18 07:24:00 -0400

    Update rolling.jl

--- src/rolling.jl
+++ src/rolling.jl
@@ -1,3 +1,5 @@
+span_error(n_in, span) = ErrorException("The data length ($n_in) is less than the window size ($span).")
+
 """
 rolling(fn, span, data)    
 applies fn to successive sub-spans of data   
@@ -5,7 +7,7 @@ length(result) == length(data) - span + 1
 """
 function rolling{T}(fn::Function, span::Int, data::Vector{T})
     n_in  = length(data)
-    (span >= 1 && n_in >= span) || throw(DomainError())
+    (span >= 1 && n_in >= span) || throw(span_error(n_in, span))
 
     n_out = n_in - span + 1
     res = zeros(T, n_out)
@@ -19,14 +21,50 @@ function rolling{T}(fn::Function, span::Int, data::Vector{T})
 end
 
 """
-rolling_fill_last(fn, span, data) is is rolling last result is carried forward      
+rolling_fill_first(fn, span, data)
+applies fn to successive sub-spans of data    
+carries the span_th result backward
+length(result) == length(data)
+"""
+function rolling_fill_first{T}(fn::Function, span::Int, data::Vector{T})
+    n_in  = length(data)
+    (span >= 1 && n_in >= span) || throw(span_error(n_in, span))
+
+    n_out = n_in - span   
+    res   = zeros(T, n_in)
+    res[span:n_out+span] = rolling(fn, span, data)
+    res[1:span-1] = res[span]
+   return res
+end
+
+"""
+rolling_fill_first(fn, span, data, filler)
+applies fn to successive sub-spans of data    
+uses filler to fill the first span-1 entries
+length(result) == length(data)
+"""
+function rolling_fill_first{T}(fn::Function, span::Int, data::Vector{T}, filler::T)
+    n_in  = length(data)
+    (span >= 1 && n_in >= span) || throw(span_error(n_in, span))
+
+    n_out = n_in - span   
+    res   = zeros(T, n_in)
+    res[span:n_out+span] = rolling(fn, span, data)
+    res[1:span-1] = filler
+   return res
+end
+
+
+"""
+rolling_fill_last(fn, span, data)
 applies fn to successive sub-spans of data    
 carries the (end-span)_th result forward
 length(result) == length(data)
 """
 function rolling_fill_last{T}(fn::Function, span::Int, data::Vector{T})
     n_in  = length(data)
-    (span >= 1 && n_in >= span) || throw(DomainError())
+    (span >= 1 && n_in >= span) || throw(span_error(n_in, span))
+
 
     n_out = n_in - span + 1   
     res   = zeros(T, n_in)
@@ -36,7 +74,25 @@ function rolling_fill_last{T}(fn::Function, span::Int, data::Vector{T})
 end
 
 """
-rolling_fill_first(fn, span, data) is rolling where first result is carried backward    
+rolling_fill_last(fn, span, data, filler)
+applies fn to successive sub-spans of data    
+uses filler to fill the last span-1 entries
+length(result) == length(data)
+"""
+function rolling_fill_last{T}(fn::Function, span::Int, data::Vector{T}, filler::T)
+    n_in  = length(data)
+    (span >= 1 && n_in >= span) || throw(span_error(n_in, span))
+
+
+    n_out = n_in - span + 1   
+    res   = zeros(T, n_in)
+    res[1:n_out] = rolling(fn, span, data)
+    res[n_out+1:end] = filler
+   return res
+end
+
+"""
+rolling_fill_first(fn, span, data)
 applies fn to successive sub-spans of data    
 carries the span_th result backward
 length(result) == length(data)
