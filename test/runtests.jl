720fe0685ac1ea400c729c2cc1629cc670a18e67
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2017-04-18 12:41:05 -0400

    Update runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -6,12 +6,12 @@ fdata = [1.0, 3.0, 5.0, 8.0,  6.0, 9.0]
 @test rolling(mean, 4, fdata)  ==  [4.25, 5.5, 7.0]
 @test rolling(mean, 3, fdata)  ==  [3.0, 5.333333333333333, 6.333333333333333, 7.666666666666667]
 
-@test rolling_mean(FILL_FIRST, 4, fdata) == [4.25, 4.25, 4.25, 4.25, 5.5, 7.0]
-@test rolling_mean(FILL_LAST, 4, fdata) == [4.25, 5.5, 7.0, 7.0, 7.0, 7.0]
-@test string(rolling_mean(FILL_FIRST, 4, NaN, fdata)) == string([NaN, NaN, NaN, 4.25, 5.5, 7.0])
-@test string(rolling_mean(FILL_LAST, 4, NaN, fdata)) == string([4.25, 5.5, 7.0, NaN, NaN, NaN])
+@test roll_mean(FILL_FIRST, 4, fdata) == [4.25, 4.25, 4.25, 4.25, 5.5, 7.0]
+@test roll_mean(FILL_LAST, 4, fdata) == [4.25, 5.5, 7.0, 7.0, 7.0, 7.0]
+@test string(roll_mean(FILL_FIRST, 4, NaN, fdata)) == string([NaN, NaN, NaN, 4.25, 5.5, 7.0])
+@test string(roll_mean(FILL_LAST, 4, NaN, fdata)) == string([4.25, 5.5, 7.0, NaN, NaN, NaN])
 
-@test rolling_mean(TAPER_FIRST, 4, 3, fdata) == [3.0, 3.0, 3.0, 4.25, 5.5, 7.0]
-@test rolling_mean(TAPER_FIRST, 4, 2, fdata) == [2.0, 2.0, 3.0, 4.25, 5.5, 7.0]
-@test rolling_mean(TAPER_LAST, 4, 3, fdata) == [4.25, 5.5, 7.0, 7.666666666666667, 7.666666666666667, 7.666666666666667]
-@test rolling_mean(TAPER_LAST, 4, 2, fdata) == [4.25, 5.5, 7.0, 7.666666666666667, 7.5, 7.5]
+@test roll_mean(TAPER_FIRST, 4, 3, fdata) == [3.0, 3.0, 3.0, 4.25, 5.5, 7.0]
+@test roll_mean(TAPER_FIRST, 4, 2, fdata) == [2.0, 2.0, 3.0, 4.25, 5.5, 7.0]
+@test roll_mean(TAPER_LAST, 4, 3, fdata) == [4.25, 5.5, 7.0, 7.666666666666667, 7.666666666666667, 7.666666666666667]
+@test roll_mean(TAPER_LAST, 4, 2, fdata) == [4.25, 5.5, 7.0, 7.666666666666667, 7.5, 7.5]
