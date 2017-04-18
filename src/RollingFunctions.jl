ece851244fa94d03138f30309e6750b1ee2a331a
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2017-04-18 10:16:38 -0400

    Update RollingFunctions.jl

--- src/RollingFunctions.jl
+++ src/RollingFunctions.jl
@@ -5,7 +5,7 @@ export roll_minimum, roll_maximum, roll_mean,
        roll_minimum_filled, roll_maximum_filled, roll_mean_filled, 
        roll_std_filled, roll_var_filled, roll_mad_filled,
        roll_minimum_tapered, roll_maximum_tapered, roll_mean_tapered,
-       rolling, rolling_fill_first, rolling_fill_last, rolling_taper_first, rolling_taper_last,
+       rolling, 
        FILL_NONE, FILL_FIRST, FILL_LAST, FILL_BOTH, TAPER_FIRST, TAPER_LAST, TAPER_BOTH
        
 using StatsBase
