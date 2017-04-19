38607017ba425652b7ff315bb628504fbd789721
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2017-04-18 22:58:21 -0400

    Update runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,4 +1,5 @@
 using RollingFunctions
+using StatsBase
 using Base.Test
 
 fdata = [1.0, 3.0, 5.0, 8.0,  6.0, 9.0]
@@ -15,3 +16,9 @@ fdata = [1.0, 3.0, 5.0, 8.0,  6.0, 9.0]
 @test roll_mean(TAPER_FIRST, 4, 2, fdata) == [2.0, 2.0, 3.0, 4.25, 5.5, 7.0]
 @test roll_mean(TAPER_LAST, 4, 3, fdata) == [4.25, 5.5, 7.0, 7.666666666666667, 7.666666666666667, 7.666666666666667]
 @test roll_mean(TAPER_LAST, 4, 2, fdata) == [4.25, 5.5, 7.0, 7.666666666666667, 7.5, 7.5]
+
+data = [ 1.0, 3.0, 5.0, 7.0, 11.0, 15.0 ]
+weights = [1/8, 2/8, 5/8]
+
+@test roll_middle(3, data) == [ 3.0,  5.0,  8.0,  11.0 ]
+@test roll_middle(3, weights, data)  == [ 1.625, 2.375, 3.75, 5.125 ]
