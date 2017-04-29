90c9d1a53105bd449a7f8b9ae8e78630ca0f7ff2
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2017-04-29 13:17:11 -0400

    Create RollingFunctions.jl

---
+++ stable/src/RollingFunctions.jl
@@ -0,0 +1,21 @@
+module RollingFunctions
+
+export roll_minimum, roll_maximum, roll_mean, 
+       roll_std, roll_var, roll_mad,       
+       rolling, 
+       FILL_FIRST, FILL_LAST, FILL_BOTH,
+       TAPER_FIRST, TAPER_LAST, TAPER_BOTH
+       
+using StatsBase
+
+const FILL_FIRST = Val{:FILL_FIRST}
+const FILL_LAST  = Val{:FILL_LAST}
+const FILL_BOTH  = Val{:FILL_BOTH}
+const TAPER_FIRST = Val{:TAPER_FIRST}
+const TAPER_LAST  = Val{:TAPER_LAST}
+const TAPER_BOTH  = Val{:TAPER_BOTH}
+
+include("rolling.jl")
+include("running.jl")
+
+end # module
