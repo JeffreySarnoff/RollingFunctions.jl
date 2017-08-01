519e70b226715dcc65c5b0097d120f77aac498c1
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2017-08-01 03:46:18 -0400

    update for v0.7.0

--- src/running.jl
+++ src/running.jl
@@ -3,25 +3,25 @@ for (F,R) in [(:minimum, :roll_minimum), (:maximum, :roll_maximum),
               (:std, :roll_std), (:var, :roll_var), (:mad, :roll_mad),
               (:sum, :roll_sum), (:prod, :roll_prod), (:norm, :roll_norm)]
     @eval begin
-        $R{T}(span::Int, data::Vector{T}) = rolling($F, span, data)
-        $R{T}(span::Int, weights::Vector{T}, data::Vector{T}) = rolling($F, span, weights, data)
-        $R{T}(::Type{FILL_FIRST}, span::Int, data::Vector{T}) =
+        $R(span::Int, data::Vector{T}) where {T} = rolling($F, span, data)
+        $R(span::Int, weights::Vector{T}, data::Vector{T}) where {T} = rolling($F, span, weights, data)
+        $R(::Type{FILL_FIRST}, span::Int, data::Vector{T}) where {T} =
             rolling(FILL_FIRST, $F, span, data)
-        $R{T}(::Type{FILL_LAST},  span::Int, data::Vector{T}) =
+        $R(::Type{FILL_LAST},  span::Int, data::Vector{T}) where {T} =
             rolling(FILL_LAST,  $F, span, data)
-        $R{T}(::Type{FILL_BOTH},  span::Int, data::Vector{T}) =
+        $R(::Type{FILL_BOTH},  span::Int, data::Vector{T}) where {T} =
             rolling(FILL_BOTH,  $F, span, data)
-        $R{T}(::Type{FILL_FIRST}, span::Int, filler::T, data::Vector{T}) =
+        $R(::Type{FILL_FIRST}, span::Int, filler::T, data::Vector{T}) where {T} =
             rolling(FILL_FIRST, $F, span, filler, data)
-        $R{T}(::Type{FILL_LAST},  span::Int, filler::T, data::Vector{T}) =
+        $R(::Type{FILL_LAST},  span::Int, filler::T, data::Vector{T}) where {T} =
             rolling(FILL_LAST,  $F, span, filler, data)
-        $R{T}(::Type{FILL_BOTH},  span::Int, filler::T, data::Vector{T}) =
+        $R(::Type{FILL_BOTH},  span::Int, filler::T, data::Vector{T}) where {T} =
             rolling(FILL_BOTH,  $F, span, filler, data)
-        $R{T}(::Type{TAPER_FIRST}, span::Int, tapered_span::Int, data::Vector{T}) =
+        $R(::Type{TAPER_FIRST}, span::Int, tapered_span::Int, data::Vector{T}) where {T} =
             rolling(TAPER_FIRST, $F, span, tapered_span, data)
-        $R{T}(::Type{TAPER_LAST},  span::Int, tapered_span::Int, data::Vector{T}) =
+        $R(::Type{TAPER_LAST},  span::Int, tapered_span::Int, data::Vector{T}) where {T} =
            rolling(TAPER_LAST,  $F, span, tapered_span, data)
-        $R{T}(::Type{TAPER_BOTH},  span::Int, tapered_span::Int, data::Vector{T}) = 
+        $R(::Type{TAPER_BOTH},  span::Int, tapered_span::Int, data::Vector{T}) where {T} = 
            rolling(TAPER_BOTH,  $F, span, tapered_span, data)                
     end
 end
