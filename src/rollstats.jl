38d5e43d47c5ece5ea3dbe9ac501a1dde906497b
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2018-05-01 08:02:26 -0400

    Update rollstats.jl

--- src/rollstats.jl
+++ src/rollstats.jl
@@ -1,1 +1,11 @@
-
+for (R,F) in ((:rollmin, :minimum), (:rollmax, :maximum),
+              (:rollmean, :mean), (:rollmedian, :median), 
+              (:rollvar, :var), (:rollstd, :std),
+              (:rollskewness, :skewness), (:rollkurtosis, :kurtosis),
+              (:rollsem, :sem), (:rollmad, :mad),
+              (:rollvariation, :variation))
+    @eval begin
+        function $R(data::AbstractVector{T}, windowspan::Int) where {T} =
+            rolling($F, data, windowspan)
+    end
+end
