945d0fbc817f5e7f871102607c0af107aca90bb3
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2018-05-01 08:11:58 -0400

    Update rolling.jl

--- src/rolling.jl
+++ src/rolling.jl
@@ -33,7 +33,7 @@ end
 
 
 rolling(fun::Function, data::AbstractVector{T}, windowspan::Int, weights::W) where
-                {T, N<:Number, W<:AbstractWeight} =
+                {T, N<:Number, W<:AbstractWeights} =
     rolling(fun, data, windowspan, weights.values)
 
 # number of values to be obtained
