adf74c660919f532e1d251ed9876bc3f7d16ad5e
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2018-05-01 10:35:53 -0400

    Update runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,4 +1,8 @@
 using StatsBase, RollingFunctions
 using Test
 
-@test 1 == 1
+
+data = [2.0, 5.0, 3.0, 2.0, 4.0, 5.0, 4.0]
+
+@test rollmean(data, 5) == rolling(mean, data, 5)
+@test runmax(data, 4) == rolling(maximum, data, 4)
