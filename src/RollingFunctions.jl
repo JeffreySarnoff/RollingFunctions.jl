49be4653ad7964220d32bba24346e379cb3e6009
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2018-05-01 08:27:11 -0400

    Update RollingFunctions.jl

--- src/RollingFunctions.jl
+++ src/RollingFunctions.jl
@@ -3,7 +3,7 @@ module RollingFunctions
 export rollmin,  rollmax,             # min, max
        rollmean, rollmedian,          # mean, median
        rollvar,  rollstd,             # variance, standard deviation
-       rollskewness, rollkurtosis     # skewness, kurtosis
+       rollskewness, rollkurtosis,    # skewness, kurtosis
        rollsem,                       # standard error of the mean
        rollmad,                       # mean absolute deviation
        rollvariation                  # coefficient of variation
