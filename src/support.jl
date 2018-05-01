2d0ab047841470ebfaa1ec2017b8a0f51c747e82
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2018-05-01 14:41:33 -0400

    Update support.jl

--- src/support.jl
+++ src/support.jl
@@ -1,3 +1,22 @@
+getcolumn(x::AbstractVector{T}, colidx::Int) where {T} =
+   colidx === 1 ? x : throw(DomainError("colidx ($colidx) > 1"))
+
+getcolumn(x::AbstractArray{T,2}, colidx::Int) where {T} =
+   1 <= colidx <= 2 ? x[:,colidx] : throw(DomainError("!(1 <= colidx ($colidx) <= 2)"))
+
+function getcolumn(x::AbstractArray{T,3}, colidx::Tuple(Int,Int)) where {T}
+   colidx = minmax(colidx[1], colidx[2])
+   if colidx === (1,2)
+      x[1,2,:]
+   elseif colidx === (1,3)
+      x[1,:,3]
+   elseif colidx === (2,3)
+      x[:,2,3]
+   else
+      throw(DomainError("bad colidx ($colidx)"))
+   end
+end
+
 if @isdefined DataFrames
    # get DataFrame column from the string form of the column id
    getcolumn(df::DataFrame, str::String) = getindex(df, Symbol(str))
