7322ed481a92d6c99944190c86d869c8fd60ffae
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2018-05-01 14:54:26 -0400

    Update support.jl

--- src/support.jl
+++ src/support.jl
@@ -19,12 +19,16 @@ end
 
 if @isdefined DataFrames
    # get DataFrame column from the string form of the column id
-   getcolumn(df::DataFrame, str::String) = getindex(df, Symbol(str))
-   getcolumn(df::DataFrame, sym::Symbol) = df[sym]
+   getcolumn(df::DataFrames.DataFrame, str::String) = getindex(df, Symbol(str))
+   getcolumn(df::DataFrames.DataFrame, sym::Symbol) = df[sym]
+else
+   println("DataFrames not defined")
 end
 
 if @isdefined TimeSeries
    # get TimeArray column from the symbol form of the column id
-   getcolumn(ta::TimeArray, sym::Symbol) = getindex(ta, string(sym)).values
-   getcolumn(ta::TimeArray, str::String) = getindex(ta, str).values
+   getcolumn(ta::TimeSeries.TimeArray, sym::Symbol) = getindex(ta, string(sym)).values
+   getcolumn(ta::TimeSeries.TimeArray, str::String) = getindex(ta, str).values
+else
+   println("TimeSeries not defined")
 end
