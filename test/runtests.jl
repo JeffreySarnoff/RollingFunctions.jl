ad07146c2a63ef90b78aa62ee6af94471c545e98
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2018-05-01 10:37:25 -0400

    Update runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -5,4 +5,4 @@ using Test
 data = [2.0, 5.0, 3.0, 2.0, 4.0, 5.0, 4.0]
 
 @test rollmean(data, 5) == rolling(mean, data, 5)
-@test runmax(data, 4) == rolling(maximum, data, 4)
+@test runmax(data, 4) == running(maximum, data, 4)
