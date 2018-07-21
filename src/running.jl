522b68c11fc81a538c6604d4c402178fc2cb2d6d
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2018-07-21 08:58:35 -0400

    Update running.jl

--- src/running.jl
+++ src/running.jl
@@ -16,7 +16,7 @@ end
 
 # weighted windowed function application that tapers
 
-function running(fun::Function, data::V, windowspan::Int, weights::F) where
+function running(fun::Function, data::V, windowspan::Int, weighting::F) where
                  {T, N<:Number, V<:AbstractVector{T}, F<:Vector{N}}
 
     ndata   = length(data)
@@ -25,13 +25,13 @@ function running(fun::Function, data::V, windowspan::Int, weights::F) where
     
     result = zeros(T, ndata)
     
-    result[1:ntapers] = tapers(fun, data[1:ntapers], weights[end-(ntapers-1):end])
+    result[1:ntapers] = tapers(fun, data[1:ntapers], weighting[end-(ntapers-1):end])
     ntapers += 1
-    result[ntapers:ndata] = rolling(fun, data, windowspan, weights)
+    result[ntapers:ndata] = rolling(fun, data, windowspan, weighting)
 
     return result
 end
 
-running(fun::Function, data::AbstractVector{T}, windowspan::Int, weights::W) where
+running(fun::Function, data::AbstractVector{T}, windowspan::Int, weighting::W) where
                 {T, N<:Number, W<:AbstractWeights} =
-    running(fun, data, windowspan, weights.values)
+    running(fun, data, windowspan, weighting.values)
