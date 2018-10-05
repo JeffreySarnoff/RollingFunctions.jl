9963de1a47089b90d27f2f032ac58fa528b4c071
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2018-10-05 02:25:05 -0400

    float(T) generalizes for Ints input

--- src/rolling.jl
+++ src/rolling.jl
@@ -3,7 +3,7 @@
 function rolling(fun::Function, data::AbstractVector{T}, windowspan::Int) where {T}
     nvals  = nrolled(length(data), windowspan)
     offset = windowspan - 1
-    result = zeros(T, nvals)
+    result = zeros(float(T), nvals)
 
     @inbounds for idx in eachindex(result)
         result[idx] = fun( view(data, idx:idx+offset) )
@@ -22,7 +22,7 @@ function rolling(fun::Function, data::V, windowspan::Int, weighting::F) where
 
     nvals  = nrolled(length(data), windowspan)
     offset = windowspan - 1
-    result = zeros(T, nvals)
+    result = zeros(float(T), nvals)
 
     @inbounds for idx in eachindex(result)
         result[idx] = fun( view(data, idx:idx+offset) .* weighting )
@@ -41,7 +41,7 @@ rolling(fun::Function, data::AbstractVector{T}, windowspan::Int, weighting::W) w
 
 function tapers(fun::Function, data::AbstractVector{T}) where {T}
     nvals  = length(data)
-    result = zeros(T, nvals)
+    result = zeros(float(T), nvals) 
 
     @inbounds for idx in nvals:-1:1
         result[idx] = fun( view(data, 1:idx) )
@@ -54,7 +54,7 @@ function tapers(fun::Function, data::AbstractVector{T}, ntocopy::Int) where {T}
     nvals  = length(data)
     ntocopy = min(nvals, max(0, ntocopy))
 
-    result = zeros(T, nvals)
+    result = zeros(float(T), nvals)
     if ntocopy > 0
        result[1:ntocopy] = data[1:ntocopy]
     end
@@ -72,7 +72,7 @@ function tapers(fun::Function, data::AbstractVector{T},
     
     ntrailing = axes(trailing_data)[1].stop
     nvals  = length(data) + ntrailing
-    result = zeros(T, nvals)
+    result = zeros(float(T), nvals)
 
     result[1:ntrailing] = trailing_data[1:ntrailing]
     ntrailing += 1
@@ -94,7 +94,7 @@ function tapers(fun::Function, data::V, weighting::F) where
     
     nweighting == nvals || throw(WeightsError(length(weighting), nvals))
     
-    result = zeros(T, nvals)
+    result = zeros(float(T), nvals)
 
     @inbounds for idx in nvals:-1:1
         wts = normalize(view(weighting, (nweighting-idx+1):nweighting))
