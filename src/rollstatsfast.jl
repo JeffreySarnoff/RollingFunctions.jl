a0641f8f6aec85f684fddb9ed2d8deb6f29fb0c5
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2018-10-05 04:03:46 -0400

    Update rollstatsfast.jl

--- src/rollstatsfast.jl
+++ src/rollstatsfast.jl
@@ -5,7 +5,7 @@
 function rolling_mean(data::V, windowspan::Int) where {T, V<:AbstractVector{T}}
     nvals  = nrolled(length(data), windowspan)
     offset = windowspan - 1
-    result = zeros(T, nvals)
+    result = zeros(float(T), nvals)
     result[1] = mean( view(data, 1:1+offset) )
 
     @inbounds for idx in Base.Iterators.drop(eachindex(result), 1)
@@ -16,30 +16,49 @@ function rolling_mean(data::V, windowspan::Int) where {T, V<:AbstractVector{T}}
 end
 
 function rolling_deltamean(data::V, windowspan::Int) where {T, V<:AbstractVector{T}}
-    nvals  = RollingFunctions.nrolled(length(data), windowspan)
+    nvals  = nrolled(length(data), windowspan)
     offset = windowspan - 1
-    means  = rolling_mean(data, windowspan)
-    result = zeros(T, nvals)
-    result[1] = zero(T)
+    meanvalue = mean( view(data, 1:1+offset) )
+    result = zeros(float(T), nvals)
+    result[1] = data[1+offset] - meanvalue
 
     @inbounds for idx in Base.Iterators.drop(eachindex(result), 1)
-       result[idx] = data[idx] - means[idx]
+       meanvalue = meanvalue + (data[idx+offset] - data[idx-1])
+       result[idx] = data[idx] - meanvalue
     end
 
     return result
 end
 
-function rolling_deltameansquared(data::V, windowspan::Int) where {T, V<:AbstractVector{T}}
-    nvals  = RollingFunctions.nrolled(length(data), windowspan)
+function rolling_deltamean_squared(data::V, windowspan::Int) where {T, V<:AbstractVector{T}}
+    nvals  = nrolled(length(data), windowspan)
     offset = windowspan - 1
-    means  = rolling_mean(data, windowspan)
-    result = zeros(T, nvals)
-    result[1] = zero(T)
+    meanvalue = mean( view(data, 1:1+offset) )
+    result = zeros(float(T), nvals)
+    result[1] = (data[1+offset] - meanvalue)^2
 
     @inbounds for idx in Base.Iterators.drop(eachindex(result), 1)
-       result[idx] = (data[idx] - means[idx])^2
+       meanvalue = meanvalue + (data[idx+offset] - data[idx-1])
+       result[idx] = (data[idx] - meanvalue)^2
     end
 
     return result
 end
+
+function rolling_variance(data::V, windowspan::Int) where {T, V<:AbstractVector{T}}
+    nvals  = nrolled(length(data), windowspan)
+    offset = windowspan - 1
+    meanvalue = mean( view(data, 1:1+offset) )
+    result = zeros(float(T), nvals)
+    deltamean_squared = (data[1+offset] - meanvalue)^2
+    result[1] = var 
+
+    @inbounds for idx in Base.Iterators.drop(eachindex(result), 1)
+       meanvalue = meanvalue + (data[idx+offset] - data[idx-1])
+       deltamean_squared = (data[idx] - meanvalue)^2
+    end
+
+    return result
+end
+
 
