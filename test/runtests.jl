8e4bce0d11cef16fe6e786bc5c407c53986dfde4
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2018-12-18 22:58:12 -0500

    Update runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -28,12 +28,12 @@ weighting = normalize([1.0, 2.0, 1.0, 2.0]);
 
 expected = [2.5, 6.0, 9.5, 10.0, 12.0]
 obtained = rolling(cov, data1, data2, 3)
-@test Float32(eps(Float64)) > abs(sum(expected .- obtained))
+@test eps(Float32) > abs(sum(expected .- obtained))
 
 expected = [0.9751641759537001, 0.9283031155706918, 0.9749534026403799, 0.9732162133211492]
 obtained = rollcor(data1, data2, 4, weighting)
-@test Float32(eps(Float64)) > abs(sum(expected .- obtained))
+@test eps(Float32) > abs(sum(expected .- obtained))
 
 expected = [0.0, 1.5, 2.5, 7.833333333333333, 15.0, 20.75, 26.5]
 obtained = runcov(data1, data2, 5)
-@test Float32(eps(Float64)) > abs(sum(expected .- obtained))
+@test eps(Float32) > abs(sum(expected .- obtained))
