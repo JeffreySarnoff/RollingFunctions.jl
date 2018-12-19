1c06a2edb1679c36d468cd1de084e55cfabbb941
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2018-12-18 21:25:29 -0500

    Update running2.jl

--- src/running2.jl
+++ src/running2.jl
@@ -11,6 +11,10 @@ function running(fun2::Function, data1::AbstractVector{T}, data2::AbstractVector
     ntapers += 1
     result[ntapers:ndata] = rolling(fun2, data1, data2, windowspan)
 
+    if isnan(result[1]) && ndata > 1
+        result[1] = result[2]
+    end
+    
     return result
 end
 
@@ -28,6 +32,10 @@ function running(fun2::Function, data1::V, data2::V, windowspan::Int, weighting:
     result[1:ntapers] = tapers2(fun2, data1[1:ntapers], data2[1:ntapers], weighting[end-(ntapers-1):end])
     ntapers += 1
     result[ntapers:ndata] = rolling(fun2, data1, data2, windowspan, weighting)
+
+    if isnan(result[1]) && ndata > 1
+        result[1] = result[2]
+    end
 
     return result
 end
