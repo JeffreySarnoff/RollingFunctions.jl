7b4d970d46779e53057c93d92bd1132e99cde68d
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2018-12-18 22:08:13 -0500

    Update running2.jl

--- src/running2.jl
+++ src/running2.jl
@@ -38,7 +38,6 @@ end
 
 function running(fun2::Function, data1::V, data2::V, windowspan::Int, weighting::F) where
                  {T, N<:Number, V<:AbstractVector{T}, F<:Vector{N}}
-
     ndata   = min(length(data1), length(data2))
     nvals   = nrolled(ndata, windowspan)
     ntapers = ndata - nvals
@@ -48,7 +47,7 @@ function running(fun2::Function, data1::V, data2::V, windowspan::Int, weighting:
     result[1:ntapers] = tapers2(fun2, data1[1:ntapers], data2[1:ntapers], weighting[end-(ntapers-1):end])
     ntapers += 1
     result[ntapers:ndata] = rolling(fun2, data1, data2, windowspan, weighting)
-
+    
     if isnan(result[1]) && ndata > 1
         result[1] = result[2]
     end
