381e9d75692250fbc9fe3c82bc10deb314157acf
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2018-12-18 21:31:02 -0500

    Update rollstats.jl

--- src/rollstats.jl
+++ src/rollstats.jl
@@ -18,3 +18,14 @@ for (R,F) in ((:rollmin, :minimum), (:rollmax, :maximum),
             rolling($F, data, windowspan, weighting.values)
     end
 end
+
+for (R,F) in ((:rollcor, :cor), (:rollcov, :cov))
+    @eval begin
+        $R(data1::AbstractVector{T}, data2::AbstractVector{T}, windowspan::Int) where {T} =
+            rolling($F, data1, data2, windowspan)
+        $R(data1::AbstractVector{T}, data2::AbstractVector{T}, windowspan::Int, weighting::AbstractVector{S}) where {T,S} =
+            rolling($F, data1, data2, windowspan, weighting)
+        $R(data1::AbstractVector{T}, data2::AbstractVector{T}, windowspan::Int, weighting::AbstractWeights) where {T} =
+            rolling($F, data1, data2, windowspan, weighting.values)
+    end
+end
