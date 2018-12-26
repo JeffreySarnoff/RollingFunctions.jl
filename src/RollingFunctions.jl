369d5ebb4de85afd188da6112e759bc109f31faa
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2018-12-26 05:50:05 +0100

    include rolling_missing.jl

--- src/RollingFunctions.jl
+++ src/RollingFunctions.jl
@@ -36,6 +36,8 @@ using StatsBase:  kurtosis, mad, sem, skewness, variation,
 
 
 include("roll/rolling.jl")
+include("roll/rolling_missing.jl")
+include("run/running.jl")
 include("run/running.jl")
 include("support.jl")
 include("roll/rollstats.jl")
