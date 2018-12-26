e2c90dc36acaf775214650649d6d032b89bebb1b
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2018-12-26 06:41:54 +0100

    with missings

--- src/run/runstats.jl
+++ src/run/runstats.jl
@@ -5,32 +5,37 @@ variation1(x) = length(x) > 1 ? variation(x) : 0.0
 skewness1(x) = length(x) > 1 ? skewness(x) : 1.0
 kurtosis1(x) = length(x) > 1 ? kurtosis(x) : -1.0
 
-
-for (R,F) in ((:runmin, :minimum), (:runmax, :maximum),
-              (:runmean, :mean), (:runmedian, :median), 
-              (:runvar, :var1), (:runstd, :std1),
-              (:runskewness, :skewness1), (:runkurtosis, :kurtosis1),
-              (:runsem, :sem1), 
-              (:runmad, :mad_not_normalized),
-              (:runmad_normalized, :mad_normalized),
-              (:runvariation, :variation1))
-    @eval begin
-        $R(data::AbstractVector{T}, windowspan::Int) where {T} =
-            running($F, data, windowspan)
-        $R(data::AbstractVector{T}, windowspan::Int, weighting::AbstractVector{S}) where {T,S} =
-            running($F, data, windowspan, weighting)
-        $R(data::AbstractVector{T}, windowspan::Int, weighting::AbstractWeights) where {T} =
-            running($F, data, windowspan, weighting.values)
+for T1 in (:T, :(Union{Missing,T}))
+  @eval begin
+    
+    for (R,F) in ((:runmin, :minimum), (:runmax, :maximum),
+                  (:runmean, :mean), (:runmedian, :median), 
+                  (:runvar, :var1), (:runstd, :std1),
+                  (:runskewness, :skewness1), (:runkurtosis, :kurtosis1),
+                  (:runsem, :sem1), 
+                  (:runmad, :mad_not_normalized),
+                  (:runmad_normalized, :mad_normalized),
+                  (:runvariation, :variation1))
+        @eval begin
+            $R(data::V, windowspan::Int) where {T, AbstractVector{$T1}} =
+                running($F, data, windowspan)
+            $R(data::V, windowspan::Int, weighting::AbstractVector{S}) where {T, AbstractVector{$T1}, S} =
+                running($F, data, windowspan, weighting)
+            $R(data::V, windowspan::Int, weighting::AbstractWeights) where {T, AbstractVector{$T1}} =
+                running($F, data, windowspan, weighting.values)
+        end
     end
-end
 
-for (R,F,V) in ((:runcor, :cor, 1), (:runcov, :cov, 0))
-    @eval begin
-        $R(data1::AbstractVector{T}, data2::AbstractVector{T}, windowspan::Int) where {T} =
-            running($F, data1, data2, windowspan, $V)
-        $R(data1::AbstractVector{T}, data2::AbstractVector{T}, windowspan::Int, weighting::AbstractVector{S}) where {T,S} =
-            running($F, data1, data2, windowspan, weighting, $V)
-        $R(data1::AbstractVector{T}, data2::AbstractVector{T}, windowspan::Int, weighting::AbstractWeights) where {T} =
-            running($F, data1, data2, windowspan, weighting.values, $V)
+    for (R,F,V) in ((:runcor, :cor, 1), (:runcov, :cov, 0))
+        @eval begin
+            $R(data1::V, data2::V, windowspan::Int) where {T, AbstractVector{$T1}} =
+                running($F, data1, data2, windowspan, $V)
+            $R(data1::V, data2::V, windowspan::Int, weighting::AbstractVector{S}) where {T, AbstractVector{$T1}, S} =
+                running($F, data1, data2, windowspan, weighting, $V)
+            $R(data1::V, data2::V, windowspan::Int, weighting::AbstractWeights) where {T, AbstractVector{$T1}} =
+                running($F, data1, data2, windowspan, weighting.values, $V)
+        end
     end
+
+  end
 end
