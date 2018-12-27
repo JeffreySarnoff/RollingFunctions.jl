5d21751b3f482fb7ee2d163524e837c3b5cbb147
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2018-12-27 03:26:54 +0100

    Update running2.jl

--- src/run/running2.jl
+++ src/run/running2.jl
@@ -81,7 +81,7 @@ for (T1, T2) in ((:T, :(float(T))), (:(Union{Missing,T}), :(Union{Missing,float(
         running(fun2, data1, data2, windowspan, weighting.values)
 
     running(fun2::Function, data1::V, data2::V, windowspan::Int, weighting::W, first::A) where
-                    {Tv, V<:AbstractVector{$T1}, A, N<:Number, W<:AbstractWeights} =
+                    {T<:Number, V<:AbstractVector{$T1}, A, N<:Number, W<:AbstractWeights} =
         running(fun2, data1, data2, windowspan, weighting.values, first)
 
   end
@@ -95,7 +95,7 @@ running(fun2::Function, data1::VT, data2::VU, windowspan::Int) where
 running(fun2::Function, data1::VU, data2::VT, windowspan::Int) where
   {T<:Number, U<:Union{Missing,T}, VT<:AbstractVector{T}, VU<:AbstractVector{U}} =
   running(fun2, data1, (VU)(data2), windowspan)
-#=
+
 running(fun2::Function, data1::VT, data2::VU, windowspan::Int, weighting::W) where
   {T<:Number, U<:Union{Missing,T}, VT<:AbstractVector{T}, VU<:AbstractVector{U}, N<:Number, W<:Vector{N}} =
   running(fun2, (VU)(data1), data2, windowspan, weighting)
@@ -109,4 +109,4 @@ running(fun2::Function, data1::VT, data2::VU, windowspan::Int, weighting::W) whe
 running(fun2::Function, data1::VU, data2::VT, windowspan::Int, weighting::W) where
   {T<:Number, U<:Union{Missing,T}, VT<:AbstractVector{T}, VU<:AbstractVector{U}, W<:AbstractWeights} = 
   running(fun2, data1, (VU)(data2), windowspan, weighting)
-=#
+
