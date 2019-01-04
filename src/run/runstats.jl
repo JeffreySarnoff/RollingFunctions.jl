c9c2e70081c3eae98163b1c08bf11fdef702f885
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2019-01-04 03:29:33 -0500

    Update runstats.jl

--- src/run/runstats.jl
+++ src/run/runstats.jl
@@ -25,6 +25,7 @@ for T1 in (:T, :(Union{Missing,T}))
     end
 end
 
+#=
 for T1 in (:T, :(Union{Missing,T}))
     for (R,F,V) in ((:runcor, :cor, 1), (:runcov, :cov, 0))
         @eval begin
@@ -35,5 +36,23 @@ for T1 in (:T, :(Union{Missing,T}))
             $R(data1::V, data2::V, windowspan::Int, weighting::AbstractWeights) where {T, V<:AbstractVector{$T1}} =
                 running($F, data1, data2, windowspan, weighting.values, $V)
         end
+    end
+end
+=#
+
+for (R,F,V) in ((:runcor, :cor, 1), (:runcov, :cov, 0))
+    @eval begin
+        $R(data1::V1, data2::V2, windowspan::Int) where
+           {T, V1<:Union{AbstractVector{T},AbstractVector{Union{Missing,T}}}, 
+               V2<:Union{AbstractVector{T},AbstractVector{Union{Missing,T}}}} =
+            running($F, data1, data2, windowspan, $V)
+        $R(data1::V1, data2::V2, windowspan::Int, weighting::AbstractVector{S}) where 
+           {T, V1<:Union{AbstractVector{T},AbstractVector{Union{Missing,T}}}, 
+               V2<:Union{AbstractVector{T},AbstractVector{Union{Missing,T}}}, S} =
+            running($F, data1, data2, windowspan, weighting, $V)
+        $R(data1::V1, data2::V2, windowspan::Int, weighting::AbstractWeights) where
+           {T, V1<:Union{AbstractVector{T},AbstractVector{Union{Missing,T}}}, 
+               V2<:Union{AbstractVector{T},AbstractVector{Union{Missing,T}}}} =
+            running($F, data1, data2, windowspan, weighting.values, $V)
     end
 end
