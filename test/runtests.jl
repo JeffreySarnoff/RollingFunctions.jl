c5c63fdfab6656edba4e4dca4a3d9241abe74441
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2019-03-25 08:36:57 -0400

    Update runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -11,17 +11,17 @@ windowsize = 3
 
 @test rollmean(datavec, windowsize) == Float32[2.0, 3.0, 4.0]
 
+expected = [2.0f0, 3.0f0, 4.0f0]
+obtained = rolling(mean, datavec, 3)
+@test Float32(eps(Float64)) > abs(sum(expected .- obtained))
+
 expected = [1.236568f0, 1.7457432f0, 2.254918f0]
 obtained = rollmean(datavec, windowsize, weighting)
-@test Float32(eps(Float64)) > abs(sum(expected .- obtained))
+@test Float32(eps(Float32)) > abs(sum(expected .- obtained))
 
-expected = [1.8171206f0, 2.884499f0, 3.9148676f0]
-obtained = rolling(geomean, datavec, 3)
-@test Float32(eps(Float64)) > abs(sum(expected .- obtained))
-
-expected = [1.0f0, 0.8944271f0, 0.7930564f0, 1.2588986f0, 1.7085882f0]
-obtained = running(geomean, datavec, 3, weighting)
-@test Float32(eps(Float64)) > abs(sum(expected .- obtained))
+expected = [1.0f0, 1.1180339f0, 1.236568f0, 1.7457432f0, 2.254918f0]
+obtained = running(mean, datavec, 3, weighting)
+@test Float32(eps(Float32)) > abs(sum(expected .- obtained))
 
 data1=[1,2,3,4,5,6,7];data2=[1,4,6,16,25,36,49];
 weighting = normalize([1.0, 2.0, 1.0, 2.0]);
