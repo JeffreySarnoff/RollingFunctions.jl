e86bad8aaed7cb2be34d0bf4c73cdc14d64e6962
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2022-10-11 11:54:19 -0400

    Update specify.jl

--- src/window/specify.jl
+++ src/window/specify.jl
@@ -1,37 +1,5 @@
-#=
-
-We have a `Basic` window type.  
-
-struct BasicWindow
-  - design elements::window_span
-  - sequential data::approach_to_data
-end
-
-Building on the BasicWindow
-- provide direction: insist 
-
-that nothing rests one the airship surface.
-Atlon the analysis
-that ease-in, ease-out
-
-Clients provide the windowlength and the data over which it moves.
-
-re are three foundational window types.
-- BasicWindow
-- TrimmedWindow
-- TaperedWindow
-- PaddedWindow
-
-each using structs as singletons
-=#
-struct FlatWindow
-end
-    
-struct BasicWindow
-end
-
-const FlatWindow = Union{BasicWindow, TaperedWindow, PaddedWindow}
-
+const BaseWindows = (:Basic, :Trim, :Taper, :Pad)
+const NestedWindows = (:Office, :Nest, :Offset)
 
 """
     specify_window(..)
