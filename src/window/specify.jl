2ea21a0cb4478ad2778c555eb48eb051ac4648b3
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2022-10-11 04:39:50 -0400

    Create specify.jl

---
+++ src/window/specify.jl
@@ -0,0 +1,12 @@
+"""
+    specify_window(..)
+
+Specify a `sliding window`. 
+- Identify the attributes best faciliate the purpose-laden window.
+- Select the behaviors that do most for the window.
+- Qualify behaviour, prefer some actions to others.
+- Resolve the available as the possible.
+"""
+function specify_window(attributes, behaviors)
+  
+end
