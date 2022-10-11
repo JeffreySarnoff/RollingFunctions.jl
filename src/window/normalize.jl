9d6dfe59050dceea96d47a6686f70007d303482e
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2022-10-11 04:27:45 -0400

    Update normalize.jl

--- src/window/normalize.jl
+++ src/window/normalize.jl
@@ -1,8 +1,3 @@
-using KahanSummation: sum_kbn
-using AccurateArithmetic: sum_oro
-
-const Sequence = Union{Vec, Tup} where {N, T, Vec<:AbstractVector{T}, Tup<:NTuple{N,T}}
-
 function normalize_weights(weights::Sequence)
     nweights = length(weights)
     iszero(nweights) && throw(ArgumentError("cannot normalize an empty sequence"))
