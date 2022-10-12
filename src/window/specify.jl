fd260a9ce8b383718017af0757845d33c77bb068
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2022-10-12 01:46:55 -0400

    Update specify.jl

--- src/window/specify.jl
+++ src/window/specify.jl
@@ -1,6 +1,41 @@
-const BaseWindows = (:Basic, :Trim, :Taper, :Pad)
-const NestedWindows = (:NestBasic, :NestTrim, :NestTaper, :NestPad)
-const OffsetWindows = (
+#=
+=#
+
+abstract type Specific end
+abstract type FocalBoundry <: Specific end
+
+struct Start <: FocalBoundry
+    at::Int
+end
+value(x::Start) = x.at
+Start(x::Start) = x
+
+struct Finish <: FocalBoundry
+    at::Int
+end
+value(x::Finish) = x.at
+Finish(x::Finish) = x
+
+lead_through(x::Start) = value(x) - 1
+trail_after::Finish) = (value(x) + 1)
+
+pad_upto_start::Bool
+    trim_upto_start::Bool
+    taper_upto_start::Bool
+    
+ 
+# with any Boundry  there may be *at most one* Behavior
+# with any Behavior there may be *at most one* Offset
+# with any Offset   there may be *at most one* Padding
+
+const Boundries  = ( :Start , :Finish )
+const Behaviors  = ( :Skip, :Trim, :Taper )
+const Affinities = ( :Offset, :Pad )
+
+const Morphology = (:Offset, :Weight)
+# what may be cogrediant
+struct Cogrediant{A, B}
+
 """
     specify_window(..)
 
@@ -13,9 +48,5 @@ Specify a `sliding window`.
 function specify_window (attributes, behaviors, houseplanting)
   
 end
-    
-"""
-     Basic /let him basx
-"""
-    
+
     
