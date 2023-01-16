140c778cf6c67422a1476da97d87cf4e89cd75dd
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2023-01-16 16:12:24 -0500

    Update support.jl

--- src/support.jl
+++ src/support.jl
@@ -59,7 +59,7 @@ union_types(x::Union) = (x.a, union_types(x.b)...)
 union_types(x::Type) = (x,)
 union_common(x::Union) = setdiff(union_types(T),(Missing,Nothing))
 commontype(x::Union) = union_common(x)[1]
-commontype(::Type{T}) = T
+commontype(::Type{T}) where {T} = T
 
 # filling
 
