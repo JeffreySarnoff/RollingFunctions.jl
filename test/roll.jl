cb43575b675ce3e7e5e3a101c1e89fdb618a6f04
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2023-01-18 02:47:36 -0500

    Update roll.jl

--- test/roll.jl
+++ test/roll.jl
@@ -1,3 +1,6 @@
+clean(x) = x
+clean(x::Missing) = Missing
+
 intvec = collect(1:10);
 floatvec = collect(1.0:10.0);
 intmat = hcat(intvec, reverse(intvec));
@@ -26,4 +29,25 @@ expected = [28.0 49.0; 35.0 42.0; 42.0 35.0; 49.0 28.0]
 @test result == expected
 @test typeof(result) == typeof(expected)
 
+# pad first
+
+result = rolling(intvec, window_span, window_fn; padding = missing)
+expected = [missing, missing, missing, missing, missing, missing, 28, 35, 42, 49]
+@test map(clean, result) == map(clean, expected)
+@test typeof(result) == typeof(expected)
+
+result = rolling(floatvec, window_span, window_fn)
+expected = [28.0, 35.0, 42.0, 49.0]
+@test map(clean, result) == map(clean, expected)
+@test typeof(result) == typeof(expected)
+
+result = rolling(intmat, window_span, window_fn)
+expected = [28 49; 35 42; 42 35; 49 28]
+@test map(clean, result) == map(clean, expected)
+@test typeof(result) == typeof(expected)
+
+result = rolling(floatmat, window_span, window_fn)
+expected = [28.0 49.0; 35.0 42.0; 42.0 35.0; 49.0 28.0]
+@test map(clean, result) == map(clean, expected)
+@test typeof(result) == typeof(expected)
 
