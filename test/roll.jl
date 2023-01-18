7cf7495630ab2a3e0009ba681d3694d9ce909262
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2023-01-18 02:51:03 -0500

    Update roll.jl

--- test/roll.jl
+++ test/roll.jl
@@ -36,18 +36,41 @@ expected = [missing, missing, missing, missing, missing, missing, 28, 35, 42, 49
 @test map(clean, result) == map(clean, expected)
 @test typeof(result) == typeof(expected)
 
-result = rolling(floatvec, window_span, window_fn)
+result = rolling(floatvec, window_span, window_fn; padding = missing)
+expected = [missing, missing, missing, missing, missing, missing, 28.0, 35.0, 42.0, 49.0]
+@test map(clean, result) == map(clean, expected)
+@test typeof(result) == typeof(expected)
+
+result = rolling(intmat, window_span, window_fn; padding = missing)
+expected = [missing missing; missing missing; missing missing; missing missing; missing missing; missing missing; 28 49; 35 42; 42 35; 49 28]
+  
+@test map(clean, result) == map(clean, expected)
+@test typeof(result) == typeof(expected)
+
+result = rolling(floatmat, window_span, window_fn; padding = missing)
+expected = [missing missing; missing missing; missing missing; missing missing; missing missing; missing missing; 28.0 49.0; 35.0 42.0; 42.0 35.0; 49.0 28.0]
+@test map(clean, result) == map(clean, expected)
+@test typeof(result) == typeof(expected)
+
+
+# pad last
+
+result = rolling(intvec, window_span, window_fn; padding = missing, padlast=true)
+expected = [missing, missing, missing, missing, missing, missing, 28, 35, 42, 49]
+@test map(clean, result) == map(clean, expected)
+@test typeof(result) == typeof(expected)
+
+result = rolling(floatvec, window_span, window_fn; padding = missing, padlast=true)
 expected = [28.0, 35.0, 42.0, 49.0]
 @test map(clean, result) == map(clean, expected)
 @test typeof(result) == typeof(expected)
 
-result = rolling(intmat, window_span, window_fn)
+result = rolling(intmat, window_span, window_fn; padding = missing, padlast=true)
 expected = [28 49; 35 42; 42 35; 49 28]
 @test map(clean, result) == map(clean, expected)
 @test typeof(result) == typeof(expected)
 
-result = rolling(floatmat, window_span, window_fn)
+result = rolling(floatmat, window_span, window_fn; padding = missing, padlast=true)
 expected = [28.0 49.0; 35.0 42.0; 42.0 35.0; 49.0 28.0]
 @test map(clean, result) == map(clean, expected)
 @test typeof(result) == typeof(expected)
-
