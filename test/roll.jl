bec95f91a3520fa2bb5f8b3e1282b53fbafda628
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2023-01-18 02:53:40 -0500

    Update roll.jl

--- test/roll.jl
+++ test/roll.jl
@@ -55,22 +55,22 @@ expected = [missing missing; missing missing; missing missing; missing missing; 
 
 # pad last
 
-result = rolling(intvec, window_span, window_fn; padding = missing, padlast=true)
+result = rolling(intvec, window_span, window_fn; padding = 0, padlast=true)
 expected = [missing, missing, missing, missing, missing, missing, 28, 35, 42, 49]
 @test map(clean, result) == map(clean, expected)
 @test typeof(result) == typeof(expected)
 
-result = rolling(floatvec, window_span, window_fn; padding = missing, padlast=true)
+result = rolling(floatvec, window_span, window_fn; padding = 0.0, padlast=true)
 expected = [28.0, 35.0, 42.0, 49.0]
 @test map(clean, result) == map(clean, expected)
 @test typeof(result) == typeof(expected)
 
-result = rolling(intmat, window_span, window_fn; padding = missing, padlast=true)
-expected = [28 49; 35 42; 42 35; 49 28]
+result = rolling(intmat, window_span, window_fn; padding = 0, padlast=true)
+expected = [28 49; 35 42; 42 35; 49 28; 0 0; 0 0; 0 0; 0 0; 0 0; 0 0]
 @test map(clean, result) == map(clean, expected)
 @test typeof(result) == typeof(expected)
 
-result = rolling(floatmat, window_span, window_fn; padding = missing, padlast=true)
-expected = [28.0 49.0; 35.0 42.0; 42.0 35.0; 49.0 28.0]
+result = rolling(floatmat, window_span, window_fn; padding = 0.0, padlast=true)
+expected = [28.0 49.0; 35.0 42.0; 42.0 35.0; 49.0 28.0; 0.0 0.0; 0.0 0.0; 0.0 0.0; 0.0 0.0; 0.0 0.0; 0.0 0.0]
 @test map(clean, result) == map(clean, expected)
 @test typeof(result) == typeof(expected)
