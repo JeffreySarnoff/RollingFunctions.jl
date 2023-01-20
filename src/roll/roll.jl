5b7bf80d3d775dffa4fee2d07f965297275e7bc9
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2023-01-20 10:55:01 -0500

    Create roll.jl

---
+++ src/roll/roll.jl
@@ -0,0 +1,45 @@
+function rolling(data1::AbstractVector{T1}, window_span::Int, window_fn::F;
+                 padding=Nothing, padfirst=true, padlast=false) where {T1, F<:Function}
+    if  isNothing(padding)
+        basic_rolling(data1, window_span, window_fn)
+    elseif !padlast
+        padded_rolling(data1, window_span, window_fn; padding)
+    else
+        last_padded_rolling(data1, window_span, window_fn; padding)
+    end
+end
+
+function rolling(data1::AbstractVector{T1}, data2::AbstractVector{T2}, window_span::Int, window_fn::F;
+                 padding=Nothing, padfirst=true, padlast=false) where {T1, T2, F<:Function}
+    if  isNothing(padding)
+        basic_rolling(data1, data2, window_span, window_fn)
+    elseif !padlast
+        padded_rolling(data1, data2, window_span, window_fn; padding)
+    else
+        last_padded_rolling(data1, data2, window_span, window_fn; padding)
+    end
+end
+
+function rolling(data1::AbstractVector{T1}, data2::AbstractVector{T2}, data3::AbstractVector{T3}, window_span::Int, window_fn::F;
+                 padding=Nothing, padfirst=true, padlast=false) where {T1, T2, T3, F<:Function}
+    if  isNothing(padding)
+        basic_rolling(data1, data2, data3, window_span, window_fn)
+    elseif !padlast
+        padded_rolling(data1, data2, data3, window_span, window_fn; padding)
+    else
+        last_padded_rolling(data1, data2, data3, window_span, window_fn; padding)
+    end
+end
+
+function rolling(data1::AbstractVector{T1}, data2::AbstractVector{T2}, data3::AbstractVector{T3}, data4::AbstractVector{T4},
+                 window_span::Int, window_fn::F;
+                 padding=Nothing, padfirst=true, padlast=false) where {T1, T2, T3, F<:Function}
+    if  isNothing(padding)
+        basic_rolling(data1, data2, data3, data4, window_span, window_fn)
+    elseif !padlast
+        padded_rolling(data1, data2, data3, data4, window_span, window_fn; padding)
+    else
+        last_padded_rolling(data1, data2, data3, data4, window_span, window_fn; padding)
+    end
+end
+
