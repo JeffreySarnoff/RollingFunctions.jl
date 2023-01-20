9064cb18cf2331183e6f33323d68c6db2571990c
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2023-01-20 13:14:44 -0500

    Update roll.jl

--- src/roll/roll.jl
+++ src/roll/roll.jl
@@ -53,3 +53,72 @@ function rolling(data1::AbstractMatrix{T1}, window_span::Int, window_fn::F;
         last_padded_rolling(data1, window_span, window_fn; padding)
     end
 end
+
+# weighted
+
+function rolling(data1::AbstractVector{T1}, window_span::Int, window_fn::F, weights::AbstractVector{TW};
+                 padding=Nothing, padfirst=true, padlast=false) where {T1, TW, F<:Function}
+    if  isNothing(padding)
+        basic_rolling(data1, window_span, window_fn, weights)
+    elseif !padlast
+        padded_rolling(data1, window_span, window_fn, weights; padding)
+    else
+        last_padded_rolling(data1, window_span, window_fn, weights; padding)
+    end
+end
+
+function rolling(data1::AbstractVector{T1}, data2::AbstractVector{T2}, window_span::Int, window_fn::F, weights::AbstractVector{TW};
+                 padding=Nothing, padfirst=true, padlast=false) where {T1, T2, TW, F<:Function}
+    if  isNothing(padding)
+        basic_rolling(data1, data2, window_span, window_fn, weights)
+    elseif !padlast
+        padded_rolling(data1, data2, window_span, window_fn, weights; padding)
+    else
+        last_padded_rolling(data1, data2, window_span, window_fn, weights; padding)
+    end
+end
+
+function rolling(data1::AbstractVector{T1}, data2::AbstractVector{T2}, data3::AbstractVector{T3}, window_span::Int, window_fn::F, weights::AbstractVector{TW};
+                 padding=Nothing, padfirst=true, padlast=false) where {T1, T2, T3, TW, F<:Function}
+    if  isNothing(padding)
+        basic_rolling(data1, data2, data3, window_span, window_fn, weights)
+    elseif !padlast
+        padded_rolling(data1, data2, data3, window_span, window_fn, weights; padding)
+    else
+        last_padded_rolling(data1, data2, data3, window_span, window_fn, weights; padding)
+    end
+end
+
+function rolling(data1::AbstractVector{T1}, data2::AbstractVector{T2}, data3::AbstractVector{T3}, data4::AbstractVector{T4},
+                 window_span::Int, window_fn::F, weights::AbstractVector{TW};
+                 padding=Nothing, padfirst=true, padlast=false) where {T1, T2, T3, T4, TW, F<:Function}
+    if  isNothing(padding)
+        basic_rolling(data1, data2, data3, data4, window_span, window_fn, weights)
+    elseif !padlast
+        padded_rolling(data1, data2, data3, data4, window_span, window_fn, weights; padding)
+    else
+        last_padded_rolling(data1, data2, data3, data4, window_span, window_fn, weights; padding)
+    end
+end
+
+function rolling(data1::AbstractMatrix{T1}, window_span::Int, window_fn::F, weights::AbstractVector{TW};
+                 padding=Nothing, padfirst=true, padlast=false) where {T1, TW, F<:Function}
+    if  isNothing(padding)
+        basic_rolling(data1, window_span, window_fn, weights)
+    elseif !padlast
+        padded_rolling(data1, window_span, window_fn, weights; padding)
+    else
+        last_padded_rolling(data1, window_span, window_fn, weights; padding)
+    end
+end
+
+function rolling(data1::AbstractMatrix{T1}, window_span::Int, window_fn::F, weights::AbstractMatrix{TW};
+                 padding=Nothing, padfirst=true, padlast=false) where {T1, TW, F<:Function}
+    if  isNothing(padding)
+        basic_rolling(data1, window_span, window_fn, weights)
+    elseif !padlast
+        padded_rolling(data1, window_span, window_fn, weights; padding)
+    else
+        last_padded_rolling(data1, window_span, window_fn, weights; padding)
+    end
+end
