7581ac76ea5f538cbf92af8256b30b1c3bd842cd
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2023-01-23 07:42:15 -0500

    Update exceptions.jl

--- src/support/exceptions.jl
+++ src/support/exceptions.jl
@@ -6,10 +6,11 @@ Base.showerror(io::IO, e::Span) = print(io, e.msg)
 span_error_msg(msg::String) = Span(msg)
 span_error(msg::String) = throw(span_error_msg(msg))
 
-mutable struct Weights <: Exception
+mutable struct Weighting <: Exception
     msg::String
 end
 
-Base.showerror(io::IO, e::Weights) = print(io, e.msg)
-weights_error_msg(msg::String) = Weights(msg)
-weights_error(msg::String) = throw(weights_error_msg(msg))
+Base.showerror(io::IO, e::Weighting) = print(io, e.msg)
+weighting_error_msg(msg::String) = Weighting(msg)
+weighting_error(msg::String) = throw(weights_error_msg(msg))
+
