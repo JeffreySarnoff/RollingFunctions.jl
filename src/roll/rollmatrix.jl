467ce1adbbc4ac5c5dcc9794275caa00f5c2c5d9
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2023-01-23 15:32:10 -0500

    Update rollmatrix.jl

--- src/roll/rollmatrix.jl
+++ src/roll/rollmatrix.jl
@@ -26,24 +26,47 @@ end
 
 # pad the dropped indicies with a given padding value
 
-function padded_rolling(window_fn::Function, data::AbstractMatrix{T}, window_span::Int;
-    padding=nothing, padfirst=true, padlast=false) where {T}
+function basic_rolling(window_fn::Function, data::AbstractMatrix{T}, window_span::Int) where {T}
     ᵛʷdata = asview(data)
-    # there are 1 or more columns, each holds `n` values
-    nvalues = nrows(ᵛʷdata)
-    rettypes  = rts.(Ref(window_fn), map(typeof, ᵛʷdata[1,:]))
+    n = nrows(ᵛʷdata)
+    nvalues  = nrolled(n, window_span) 
+    rettype  = rts(window_fn, (eltype(ᵛʷdata),))
+    results = Matrix{rettype}(undef, (nvalues, ncols(ᵛʷdata)))
+
+    ilow, ihigh = 1, window_span
+    @inbounds for idx in eachindex(eachrow(results))
+        @views results[idx, :] .= map(window_fn, eachcol(data[ilow:ihigh, :]))
+        ilow = ilow + 1
+        ihigh = ihigh + 1
+    end
+
+    results
+end
+
+function padded_rolling(window_fn::Function, data::AbstractMatrix{T}, window_span::Int;
+                        padding=nothing, padfirst=true, padlast=false) where {T}
+    ᵛʷdata = asview(data)
+    n = nrows(ᵛʷdata)
+    nvalues  = nrolled(n, window_span) 
+    rettype  = Union{typeof(padding), rts(window_fn, (eltype(ᵛʷdata),))}
+    results = Matrix{rettype}(undef, (nvalues, ncols(ᵛʷdata)))
     
     # only completed window_span coverings are resolvable
     # the first (window_span - 1) values are unresolved wrt window_fn
     # this is the padding_span
-    padding_span = window_span - 1
-  
+    padding_span = window_span - 1  
     padding_idxs = 1:padding_span
-    ilow, ihigh = 1, window_span
 
     results = Matrix{Union{typeof(padding), rettypes}}(undef, size(ᵛʷdata))  
     results[padding_idxs, :] .= padding
 
+    ilow, ihigh = 1, window_span
+    @inbounds for idx in eachindex(eachrow(results))
+        @views results[idx, :] .= map(window_fn, eachcol(data[ilow:ihigh, :]))
+        ilow = ilow + 1
+        ihigh = ihigh + 1
+    end
+     
     @inbounds for idx in window_span:nvalues
         @views results[idx, :] = map(window_fn, eachcol(ᵛʷdata[ilow:ihigh, :]))
         ilow = ilow + 1
