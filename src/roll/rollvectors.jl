59205850e44a8eafeed64d8852cc25c7e9d8d4fc
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2023-01-23 09:39:47 -0500

    Update rollvectors.jl

--- src/roll/rollvectors.jl
+++ src/roll/rollvectors.jl
@@ -32,21 +32,9 @@ function basic_rolling(window_fn::Function, data1::AbstractVector{T1}, data2::Ab
     ᵛʷdata2 = asview(data2)
     nvalues  = nrolled(min(length(ᵛʷdata1),length(ᵛʷdata2)), window_span)
     rettype  = rts(window_fn, (typeof(ᵛʷdata1), typeof(ᵛʷdata2)))
-
-    # only completed window_span coverings are resolvable
-    # the first (window_span - 1) values are unresolved wrt window_fn
-    unresolved = window_span - 1
-
-    # with the next value, a full window_span is obtained
-    # only then is the first window_covered_value determined
-    # with each next value (with successive indices), an updated
-    # full window_span obtains, covering another window_fn value
-    window_covered_values = nvalues - unresolved
-
-    results = Vector{rettype}(undef, window_covered_values)
+    results = Vector{rettype}(undef, nvalues)
 
     ilow, ihigh = 1, window_span
-
     @inbounds for idx in eachindex(results)
         @views results[idx] = window_fn(ᵛʷdata1[ilow:ihigh], ᵛʷdata2[ilow:ihigh])
         ilow = ilow + 1
@@ -63,21 +51,9 @@ function basic_rolling(window_fn::Function, data1::AbstractVector{T1}, data2::Ab
     ᵛʷdata3 = asview(data3)
     nvalues  = nrolled(min(length(ᵛʷdata1),length(ᵛʷdata2),length(ᵛʷdata3)), window_span)
     rettype  = rts(window_fn, (typeof(ᵛʷdata1), typeof(ᵛʷdata2), typeof(ᵛʷdata3)))
-
-    # only completed window_span coverings are resolvable
-    # the first (window_span - 1) values are unresolved wrt window_fn
-    unresolved = window_span - 1
-
-    # with the next value, a full window_span is obtained
-    # only then is the first window_covered_value determined
-    # with each next value (with successive indices), an updated
-    # full window_span obtains, covering another window_fn value
-    window_covered_values = nvalues - unresolved
-
-    results = Vector{rettype}(undef, window_covered_values)
+    results = Vector{rettype}(undef, nvalues)
 
     ilow, ihigh = 1, window_span
-
     @inbounds for idx in eachindex(results)
         @views results[idx] = window_fn(ᵛʷdata1[ilow:ihigh], ᵛʷdata2[ilow:ihigh], ᵛʷdata3[ilow:ihigh])
         ilow = ilow + 1
@@ -95,21 +71,9 @@ function basic_rolling(window_fn::Function, data1::AbstractVector{T1}, data2::Ab
     ᵛʷdata4 = asview(data4)
     nvalues  = nrolled(min(length(ᵛʷdata1),length(ᵛʷdata2),length(ᵛʷdata3),length(ᵛʷdata4)), window_span)
     rettype  = rts(window_fn, (typeof(ᵛʷdata1), typeof(ᵛʷdata2), typeof(ᵛʷdata3), typeof(ᵛʷdata4)))
-
-    # only completed window_span coverings are resolvable
-    # the first (window_span - 1) values are unresolved wrt window_fn
-    unresolved = window_span - 1
-
-    # with the next value, a full window_span is obtained
-    # only then is the first window_covered_value determined
-    # with each next value (with successive indices), an updated
-    # full window_span obtains, covering another window_fn value
-    window_covered_values = nvalues - unresolved
-
-    results = Vector{rettype}(undef, window_covered_values)
+    results = Vector{rettype}(undef, nvalues)
 
     ilow, ihigh = 1, window_span
-
     @inbounds for idx in eachindex(results)
         @views results[idx] = window_fn(ᵛʷdata1[ilow:ihigh], ᵛʷdata2[ilow:ihigh], ᵛʷdata3[ilow:ihigh], ᵛʷdata3[ilow:ihigh])
         ilow = ilow + 1
