1fb951a213ed1c4a0f1ef58ae65dc8acfcc39187
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2023-01-23 08:41:27 -0500

    Update rollvectors.jl

--- src/roll/rollvectors.jl
+++ src/roll/rollvectors.jl
@@ -15,21 +15,9 @@ function basic_rolling(window_fn::Function, data1::AbstractVector{T1}, window_sp
     ᵛʷdata1 = asview(data1)
     nvalues  = nrolled(length(ᵛʷdata1), window_span)
     rettype  = rts(window_fn, (typeof(ᵛʷdata1),))
-
-    # only completed window_span coverings are resolvable
-    # the first (window_span - 1) values are unresolved wrt window_fn
-    unresolved = window_span - 1
-
-    # with the next value, a full window_span is obtained
-    # only then is the first window_covered_value determined
-    # with each next value (with successive indices), an updated
-    # full window_span obtains, covering another window_fn value
-    window_covered_values = nvalues - unresolved
-
-    results = Vector{rettype}(undef, window_covered_values)
+    results = Vector{rettype}(undef, nvalues)
 
     ilow, ihigh = 1, window_span
-
     @inbounds for idx in eachindex(results)
         @views results[idx] = window_fn(ᵛʷdata1[ilow:ihigh])
         ilow = ilow + 1
