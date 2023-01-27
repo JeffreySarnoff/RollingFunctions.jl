7e690d0da00e1155aafbb0ca3e476011ae2b104c
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2023-01-27 16:36:29 -0500

    Update exceptions.jl

--- src/support/exceptions.jl
+++ src/support/exceptions.jl
@@ -1,16 +1,66 @@
-mutable struct Span <: Exception
+#=
+    span_error
+    tile_error
+    trimspan_error
+    trimtile_error
+    weights_error
+ 
+=#
+
+struct SpanError <: Exception
     msg::String
 end
 
-Base.showerror(io::IO, e::Span) = print(io, e.msg)
-span_error_msg(msg::String) = Span(msg)
-span_error(msg::String) = throw(span_error_msg(msg))
-
-mutable struct Weighting <: Exception
+struct TileError <: Exception
     msg::String
 end
 
-Base.showerror(io::IO, e::Weighting) = print(io, e.msg)
-weighting_error_msg(msg::String) = Weighting(msg)
-weighting_error(msg::String) = throw(weights_error_msg(msg))
+struct WeightsError <: Exception
+    msg::String
+end
+
+check_span(seqlength, windowspan) =
+    windowspan > seqlength && SpanError(seqlength, windowspan)
+
+check_trimspan(seqlength, windowspan) =
+    windowspan > (seqlength - windowspan + 1) && TrimSpanError(seqlength, windowspan)
+
+check_tile(seqlength, tilespan) =
+    tilespan > seqlength && TileError(seqlength, tilespan)
+
+check_tilespan(seqlength, tilespan) =
+    tilespan > (seqlength - windowspan + 1) && TrimSpanError(seqlength, tilespan)
+
+check_weights(nweights, windowspan) =
+    nweights != windowspan && WeightsError(nweights, windowspan)
+
+function span_error(seqlength, windowspan)
+    str = string("Bad window span (", windowspan, ") for length (", seqlength, ").")
+    err = SpanError(str)
+    throw(err)
+end
+
+function trimspan_error(seqlength, windowspan)
+    str = string("Bad window span (", windowspan, ") for trimmed length (", seqlength, ").")
+    err = SpanError(str)
+    throw(err)
+end
+
+function tile_error(seqlength, tilespan)
+    str = string("Bad tile span (", tilespan, ") for length (", seqlength, ").")
+    err = TileError(str)
+    throw(err)
+end
+
+function trimtile_error(seqlength, tilespan)
+    str = string("Bad tile span (", tilespan, ") for trimmed length (", seqlength, ").")
+    err = TileError(str)
+    throw(err)
+end
+
+function weights_error(nweighting, windowspan)
+    str = string("Bad weights length (", nweighting, ") != for window length (", windowspan, ").")
+    err = WeightsError(str)
+    throw(err)
+end
 
