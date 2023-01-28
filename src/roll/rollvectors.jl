a08ef3913c6a83828811b829b4e8c7d13dfed0aa
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2023-01-28 01:27:37 -0500

    Update rollvectors.jl

--- src/roll/rollvectors.jl
+++ src/roll/rollvectors.jl
@@ -7,15 +7,6 @@
 
    last_padded_rolling(window_fn, data1, window_span; padding, padfirst, padlast) ..
    last_padded_rolling(window_fn,data1, data2, data3, data4, window_span; padding, padfirst, padlast)
-
-   basic_rolling(window_fn, data1, window_span, weights) ..
-   basic_rolling(window_fn, data1, data2, data3, data4, window_span, weights)
-
-   padded_rolling(window_fn, data1, window_span, weights; padding, padfirst, padlast) ..
-   padded_rolling(window_fn, data1, data2, data3, data4, window_span, weights; padding, padfirst, padlast)
-   
-   last_padded_rolling(window_fn, data1, window_span, weights; padding, padfirst, padlast) ..
-   last_padded_rolling(window_fn, data1, data2, data3, data4, window_span, weights; padding, padfirst, padlast)
 =#
 
 function basic_rolling(window_fn::Function, data1::AbstractVector{T1}, window_span::Int) where {T1}
