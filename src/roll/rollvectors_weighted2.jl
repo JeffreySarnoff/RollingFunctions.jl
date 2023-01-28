36a17389e8c654805e8599a126d2e9c461cce86d
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2023-01-27 22:15:10 -0500

    Create rollvectors_weighted2.jl

---
+++ src/roll/rollvectors_weighted2.jl
@@ -0,0 +1,10 @@
+#=
+   basic_rolling(window_fn, data1, window_span, weights) ..
+   basic_rolling(window_fn, data1, data2, data3, data4, window_span, weights)
+
+   padded_rolling(window_fn, data1, window_span, weights; padding, padfirst, padlast) ..
+   padded_rolling(window_fn, data1, data2, data3, data4, window_span, weights; padding, padfirst, padlast)
+   
+   last_padded_rolling(window_fn, data1, window_span, weights; padding, padfirst, padlast) ..
+   last_padded_rolling(window_fn, data1, data2, data3, data4, window_span, weights; padding, padfirst, padlast)
+=@
