0334cccdc1818348e0ad588055626521fb42f118
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2023-01-27 22:09:04 -0500

    Update rollvectors.jl

--- src/roll/rollvectors.jl
+++ src/roll/rollvectors.jl
@@ -236,14 +236,14 @@ function last_padded_rolling(window_fn::Function, data1::AbstractVector{T1}, dat
     # only completed window_span coverings are resolvable
     # the first (window_span - 1) values are unresolved wrt window_fn
     padding_span = window_span - 1
-    padding_idxs = nvalues-padding_span:nvalues
+    padding_idxs = n-padding_span-1:n
  
     rettype  = rts(window_fn, (Vector{eltype(ᵛʷdata1)}, Vector{eltype(ᵛʷdata2)}))
     results = Vector{Union{typeof(padding), rettype}}(undef, nvalues)
     results[padding_idxs] .= padding
 
     ilow, ihigh = 1, window_span
-    @inbounds for idx in 1:nvalues-padding_span
+    @inbounds for idx in 1:nvalues
         @views results[idx] = window_fn(ᵛʷdata1[ilow:ihigh], ᵛʷdata2[ilow:ihigh])
         ilow = ilow + 1
         ihigh = ihigh + 1
@@ -263,14 +263,14 @@ function last_padded_rolling(window_fn::Function, data1::AbstractVector{T1}, dat
     # only completed window_span coverings are resolvable
     # the first (window_span - 1) values are unresolved wrt window_fn
     padding_span = window_span - 1
-    padding_idxs = nvalues-padding_span:nvalues
+    padding_idxs = n-padding_span-1:n
    
     rettype  = rts(window_fn, (Vector{eltype(ᵛʷdata1)}, Vector{eltype(ᵛʷdata2)}, Vector{eltype(ᵛʷdata3)}))
     results = Vector{Union{typeof(padding), rettype}}(undef, nvalues)
     results[padding_idxs] .= padding
 
     ilow, ihigh = 1, window_span
-    @inbounds for idx in 1:nvalues-padding_span
+    @inbounds for idx in 1:nvalues
         @views results[idx] = window_fn(ᵛʷdata1[ilow:ihigh], ᵛʷdata2[ilow:ihigh], ᵛʷdata3[ilow:ihigh])
         ilow = ilow + 1
         ihigh = ihigh + 1
@@ -291,14 +291,14 @@ function last_padded_rolling(window_fn::Function, data1::AbstractVector{T1}, dat
     # only completed window_span coverings are resolvable
     # the first (window_span - 1) values are unresolved wrt window_fn
     padding_span = window_span - 1
-    padding_idxs = nvalues-padding_span:nvalues
+    padding_idxs = n-padding_span-1:n
    
     rettype  = rts(window_fn, (Vector{eltype(ᵛʷdata1)}, Vector{eltype(ᵛʷdata2)}, Vector{eltype(ᵛʷdata3)}, Vector{eltype(ᵛʷdata4)}))
     results = Vector{Union{typeof(padding), rettype}}(undef, nvalues)
     results[padding_idxs] .= padding
 
     ilow, ihigh = 1, window_span
-    @inbounds for idx in 1:nvalues-padding_span
+    @inbounds for idx in 1:nvalues
         @views results[idx] = window_fn(ᵛʷdata1[ilow:ihigh], ᵛʷdata2[ilow:ihigh], ᵛʷdata3[ilow:ihigh], ᵛʷdata4[ilow:ihigh])
         ilow = ilow + 1
         ihigh = ihigh + 1
@@ -307,115 +307,3 @@ function last_padded_rolling(window_fn::Function, data1::AbstractVector{T1}, dat
     results
 end 
 
-
-# pad last
-
-function last_padded_rolling(window_fn::Function, data1::AbstractVector{T1},
-                             window_span::Int; padding=Nothing, padfirst=true, padlast=false) where {T1}
-    ᵛʷdata1 = asview(data1)
-    nvalues  = nrolled(length(ᵛʷdata1), window_span)
-    rettype  = rts(window_fn, (Vector{eltype(ᵛʷdata1)},))
- 
-    # only completed window_span coverings are resolvable
-    # the first (window_span - 1) values are unresolved wrt window_fn
-    # this is the padding_span
-    padding_span = window_span - 1
-
-    padding_idxs = nvalues-padding_span:nvalues
-    ilow, ihigh = 1, window_span
-
-    results = Vector{Union{typeof(padding), rettype}}(undef, nvalues)
-    results[padding_idxs] .= padding
-
-    @inbounds for idx in 1:nvalues-padding_span
-        @views results[idx] = window_fn(ᵛʷdata1[ilow:ihigh])
-        ilow = ilow + 1
-        ihigh = ihigh + 1
-    end
-
-    results
-end 
-
-function last_padded_rolling(window_fn::Function, data1::AbstractVector{T1}, data2::AbstractVector{T2},
-                             window_span::Int; padding=Nothing, padfirst=true, padlast=false) where {T1,T2}
-    ᵛʷdata1 = asview(data1)
-    ᵛʷdata2 = asview(data2)
-    nvalues  = nrolled(min(length(ᵛʷdata1),length(ᵛʷdata2)), window_span)
-    rettype  = rts(window_fn, (Vector{eltype(ᵛʷdata1)}, Vector{eltype(ᵛʷdata2)}))
-
-    # only completed window_span coverings are resolvable
-    # the first (window_span - 1) values are unresolved wrt window_fn
-    # this is the padding_span
-    padding_span = window_span - 1
-
-    padding_idxs = nvalues-padding_span:nvalues
-    ilow, ihigh = 1, window_span
-
-    results = Vector{Union{typeof(padding), rettype}}(undef, nvalues)
-    results[padding_idxs] .= padding
-
-    @inbounds for idx in 1:nvalues-padding_span
-        @views results[idx] = window_fn(ᵛʷdata1[ilow:ihigh], ᵛʷdata2[ilow:ihigh])
-        ilow = ilow + 1
-        ihigh = ihigh + 1
-    end
-
-    results
-end 
-
-function last_padded_rolling(window_fn::Function, data1::AbstractVector{T1}, data2::AbstractVector{T2}, data3::AbstractVector{T3},
-                             window_span::Int; padding=Nothing, padfirst=true, padlast=false) where {T1,T2,T3}
-    ᵛʷdata1 = asview(data1)
-    ᵛʷdata2 = asview(data2)
-    ᵛʷdata3 = asview(data3)
-    nvalues  = nrolled(min(length(ᵛʷdata1),length(ᵛʷdata2),length(ᵛʷdata3)), window_span)
-    rettype  = rts(window_fn, (Vector{eltype(ᵛʷdata1)}, Vector{eltype(ᵛʷdata2)}, Vector{eltype(ᵛʷdata3)}))
-
-    # only completed window_span coverings are resolvable
-    # the first (window_span - 1) values are unresolved wrt window_fn
-    # this is the padding_span
-    padding_span = window_span - 1
-
-    padding_idxs = nvalues-padding_span:nvalues
-    ilow, ihigh = 1, window_span
-
-    results = Vector{Union{typeof(padding), rettype}}(undef, nvalues)
-    results[padding_idxs] .= padding
-
-    @inbounds for idx in 1:nvalues-padding_span
-        @views results[idx] = window_fn(ᵛʷdata1[ilow:ihigh], ᵛʷdata2[ilow:ihigh], ᵛʷdata3[ilow:ihigh])
-        ilow = ilow + 1
-        ihigh = ihigh + 1
-    end
-
-    results
-end
-
-function last_padded_rolling(window_fn::Function, data1::AbstractVector{T1}, data2::AbstractVector{T2}, data3::AbstractVector{T3}, data4::AbstractVector{T4},
-                             window_span::Int; padding=Nothing, padfirst=true, padlast=false) where {T1,T2,T3,T4}
-    ᵛʷdata1 = asview(data1)
-    ᵛʷdata2 = asview(data2)
-    ᵛʷdata3 = asview(data3)
-    ᵛʷdata4 = asview(data4)
-    nvalues  = nrolled(min(length(ᵛʷdata1),length(ᵛʷdata2),length(ᵛʷdata3),length(ᵛʷdata4)), window_span)
-    rettype  = rts(window_fn, (Vector{eltype(ᵛʷdata1)}, Vector{eltype(ᵛʷdata2)}, Vector{eltype(ᵛʷdata3)}, Vector{eltype(ᵛʷdata4)}))
-
-    # only completed window_span coverings are resolvable
-    # the first (window_span - 1) values are unresolved wrt window_fn
-    # this is the padding_span
-    padding_span = window_span - 1
-
-    padding_idxs = nvalues-padding_span:nvalues
-    ilow, ihigh = 1, window_span
-
-    results = Vector{Union{typeof(padding), rettype}}(undef, nvalues)
-    results[padding_idxs] .= padding
-
-    @inbounds for idx in 1:nvalues-padding_span
-        @views results[idx] = window_fn(ᵛʷdata1[ilow:ihigh], ᵛʷdata2[ilow:ihigh], ᵛʷdata3[ilow:ihigh], ᵛʷdata4[ilow:ihigh])
-        ilow = ilow + 1
-        ihigh = ihigh + 1
-    end
-
-    results
-end 
