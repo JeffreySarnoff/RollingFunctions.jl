6b820b8fdb77b22b15d9b95e8bf7932e4c15b402
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2023-01-27 21:39:50 -0500

    Update rollvectors.jl

--- test/rollvectors.jl
+++ test/rollvectors.jl
@@ -1,17 +1,16 @@
-S = sum; W = 3;
+F = sum; W = 3;
 D = [1, 2, 3, 4, 5];
 expected = [6, 9, 12];
-@test rolling(S, D, W) == expected
+@test rolling(F, D, W) == expected
 
 D = Float32[1, 2, 3, 4, 5];
 expected = Float32[6, 9, 12];
-@test rolling(S, D, W) == expected
+@test rolling(F, D, W) == expected
 
-S = sum; W = 4;
+F = sum; W = 4;
 D = [1, 2, 3, 4, 5];
 expected = [10, 14];
-@test rolling(S, D, W) == expected
-
+@test rolling(F, D, W) == expected
 
 D₁ = [1, 2, 3, 4, 5]
 D₂ = [5, 4, 3, 2, 1]
