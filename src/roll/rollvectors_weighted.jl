1886468398ae34a343f18372cf7241906201ba66
Jeffrey Sarnoff <jeffrey.sarnoff@gmail.com>
2023-02-01 11:53:51 -0500

    nvalues -> n

--- src/roll/rollvectors_weighted.jl
+++ src/roll/rollvectors_weighted.jl
@@ -142,7 +142,7 @@ function padded_rolling(window_fn::Function, data1::AbstractVector{T}, data2::Ab
     padding_idxs = nvalues-padding_span:nvalues
  
     rettype  = rts(window_fn, (Vector{eltype(ᵛʷdata1)}, Vector{eltype(ᵛʷdata2)}))
-    results = Vector{Union{typeof(padding), rettype}}(undef, nvalues)
+    results = Vector{Union{typeof(padding), rettype}}(undef, n)
     results[padding_idxs] .= padding
 
     ilow, ihigh = 1, window_span
@@ -171,7 +171,7 @@ function padded_rolling(window_fn::Function, data1::AbstractVector{T}, data2::Ab
     padding_idxs = nvalues-padding_span:nvalues
    
     rettype  = rts(window_fn, (Vector{eltype(ᵛʷdata1)}, Vector{eltype(ᵛʷdata2)}, Vector{eltype(ᵛʷdata3)}))
-    results = Vector{Union{typeof(padding), rettype}}(undef, nvalues)
+    results = Vector{Union{typeof(padding), rettype}}(undef, n)
     results[padding_idxs] .= padding
 
     ilow, ihigh = 1, window_span
@@ -201,7 +201,7 @@ function padded_rolling(window_fn::Function, data1::AbstractVector{T}, data2::Ab
     padding_idxs = nvalues-padding_span:nvalues
    
     rettype  = rts(window_fn, (Vector{eltype(ᵛʷdata1)}, Vector{eltype(ᵛʷdata2)}, Vector{eltype(ᵛʷdata3)}, Vector{eltype(ᵛʷdata4)}))
-    results = Vector{Union{typeof(padding), rettype}}(undef, nvalues)
+    results = Vector{Union{typeof(padding), rettype}}(undef, n)
     results[padding_idxs] .= padding
 
     ilow, ihigh = 1, window_span
@@ -258,7 +258,7 @@ function last_padded_rolling(window_fn::Function, data1::AbstractVector{T}, data
     padding_idxs = n-padding_span-1:n
  
     rettype  = rts(window_fn, (Vector{eltype(ᵛʷdata1)}, Vector{eltype(ᵛʷdata2)}))
-    results = Vector{Union{typeof(padding), rettype}}(undef, nvalues)
+    results = Vector{Union{typeof(padding), rettype}}(undef, n)
     results[padding_idxs] .= padding
 
     ilow, ihigh = 1, window_span
@@ -287,7 +287,7 @@ function last_padded_rolling(window_fn::Function, data1::AbstractVector{T}, data
     padding_idxs = n-padding_span-1:n
    
     rettype  = rts(window_fn, (Vector{eltype(ᵛʷdata1)}, Vector{eltype(ᵛʷdata2)}, Vector{eltype(ᵛʷdata3)}))
-    results = Vector{Union{typeof(padding), rettype}}(undef, nvalues)
+    results = Vector{Union{typeof(padding), rettype}}(undef, n)
     results[padding_idxs] .= padding
 
     ilow, ihigh = 1, window_span
@@ -317,7 +317,7 @@ function last_padded_rolling(window_fn::Function, data1::AbstractVector{T}, data
     padding_idxs = n-padding_span-1:n
    
     rettype  = rts(window_fn, (Vector{eltype(ᵛʷdata1)}, Vector{eltype(ᵛʷdata2)}, Vector{eltype(ᵛʷdata3)}, Vector{eltype(ᵛʷdata4)}))
-    results = Vector{Union{typeof(padding), rettype}}(undef, nvalues)
+    results = Vector{Union{typeof(padding), rettype}}(undef, n)
     results[padding_idxs] .= padding
 
     ilow, ihigh = 1, window_span
