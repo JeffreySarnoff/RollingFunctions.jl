34b263913a7c91be480e6db7ddecfb332a38269e
Jeffrey Sarnoff <jeffrey.sarnoff@gmail.com>
2023-02-02 04:47:36 -0500

    cleanup

--- src/run/runvectors.jl
+++ src/run/runvectors.jl
@@ -55,55 +55,6 @@ function basic_running(window_fn::Function,
     results
 end
 
-function basic_running(window_fn::Function,
-    data1::AbstractVector{T}, data2::AbstractVector{T},
-    window_span::Int; padding::AbstractVector{T}) where {T}
-    ᵛʷdata1 = asview(data1)
-    ᵛʷdata2 = asview(data2)
-    ᵛʷpadding = asview(padding)
-    n = min(length(ᵛʷdata1), length(ᵛʷdata2))
-    npads = length(padding)
-    nvalues = nrolled(n, window_span)
-    ntapers = n - nvalues - npads
-
-    rettype = rts(window_fn, (Vector{T},))
-    results = Vector{rettype}(undef, n)
-
-    results[1:npads] .= ᵛʷpadding
-    @inbounds for idx in npads+1:npads+ntapers
-        @views results[idx] = window_fn(ᵛʷdata1[1:idx], ᵛʷdata2[1:idx])
-    end
-
-    ilow, ihigh = 1, window_span
-    @inbounds for idx in npads+ntapers+1:n
-        @views results[idx] = window_fn(ᵛʷdata1[ilow:ihigh], ᵛʷdata1[ilow:ihigh])
-        ilow = ilow + 1
-        ihigh = ihigh + 1
-    end
-
-    results
-end
-
-
-function basic_running(window_fn::Function, data1::AbstractVector{T}, data2::AbstractVector{T}, window_span::Int) where {T}
-    ᵛʷdata1 = asview(data1)
-    ᵛʷdata2 = asview(data2)
-    n = min(length(ᵛʷdata1),length(ᵛʷdata2))
-    nvalues  = nrolled(n, window_span)
-   
-    rettype  = rts(window_fn, (Vector{T}, Vector{T}))
-    results = Vector{rettype}(undef, nvalues)
-
-    ilow, ihigh = 1, window_span
-    @inbounds for idx in eachindex(results)
-        @views results[idx] = window_fn(ᵛʷdata1[ilow:ihigh], ᵛʷdata2[ilow:ihigh])
-        ilow = ilow + 1
-        ihigh = ihigh + 1
-    end
-
-    results
-end
-
 function basic_running(window_fn::Function, data1::AbstractVector{T}, data2::AbstractVector{T}, data3::AbstractVector{T}, 
                        window_span::Int) where {T}
     ᵛʷdata1 = asview(data1)
@@ -151,18 +102,18 @@ end
 # pad first
 
 function padded_running(window_fn::Function, data1::AbstractVector{T},
-                        window_span::Int; padding=Nothing) where {T}
+    window_span::Int; padding=Nothing) where {T}
     ᵛʷdata1 = asview(data1)
     n = length(ᵛʷdata1)
 
-    nvalues  = nrolled(n, window_span) 
+    nvalues = nrolled(n, window_span)
     # only completed window_span coverings are resolvable
     # the first (window_span - 1) values are unresolved wrt window_fn
     padding_span = window_span - 1
     padding_idxs = nvalues-padding_span:nvalues
 
-    rettype  = rts(window_fn, (Vector{T},))
-    results = Vector{Union{typeof(padding), rettype}}(undef, n)
+    rettype = rts(window_fn, (Vector{T},))
+    results = Vector{Union{typeof(padding),rettype}}(undef, n)
     results[padding_idxs] .= padding
 
     ilow, ihigh = 1, window_span
@@ -175,6 +126,36 @@ function padded_running(window_fn::Function, data1::AbstractVector{T},
     results
 end 
 
+#=
+function padded_running(window_fn::Function,
+    data1::AbstractVector{T}, data2::AbstractVector{T},
+    window_span::Int; padding::AbstractVector{T}) where {T}
+    ᵛʷdata1 = asview(data1)
+    ᵛʷdata2 = asview(data2)
+    ᵛʷpadding = asview(padding)
+    n = min(length(ᵛʷdata1), length(ᵛʷdata2))
+    npads = length(padding)
+    nvalues = nrolled(n, window_span)
+    ntapers = n - nvalues - npads
+
+    rettype = rts(window_fn, (Vector{T},))
+    results = Vector{rettype}(undef, n)
+
+    results[1:npads] .= ᵛʷpadding
+    @inbounds for idx in npads+1:npads+ntapers
+        @views results[idx] = window_fn(ᵛʷdata1[1:idx], ᵛʷdata2[1:idx])
+    end
+
+    ilow, ihigh = 1, window_span
+    @inbounds for idx in npads+ntapers+1:n
+        @views results[idx] = window_fn(ᵛʷdata1[ilow:ihigh], ᵛʷdata1[ilow:ihigh])
+        ilow = ilow + 1
+        ihigh = ihigh + 1
+    end
+
+    results
+end
+=#
 
 function padded_running(window_fn::Function, data1::AbstractVector{T}, data2::AbstractVector{T},
     window_span::Int; padding=Nothing) where {T}
