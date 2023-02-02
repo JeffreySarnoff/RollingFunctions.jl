552387f07ef82cf225e8c23d201791e1d63bb167
Jeffrey Sarnoff <jeffrey.sarnoff@gmail.com>
2023-02-02 10:31:43 -0500

    use LoopVectorization

--- src/RollingFunctions.jl
+++ src/RollingFunctions.jl
@@ -38,8 +38,7 @@ using LoopVectorization
 
 for T in (:Int8, :Int16, :Int32, :Int64, :Int128,
           :UInt8, :UInt16, :UInt32, :UInt64, :UInt128,
-          :Float16, :Float32, :Float64,
-          :ComplexF16, :ComplexF32, :ComplexF64)
+          :Float16, :Float32, :Float64)
   @eval LoopVectorization.check_args(x::Union{Missing,$T}) = true
 end
 
