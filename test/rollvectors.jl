6e67bc9ff39010407e0bfbadf0d375156083a7de
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2023-02-16 14:05:14 +0000

    add EOL semi-colons

--- test/rollvectors.jl
+++ test/rollvectors.jl
@@ -25,11 +25,11 @@ D = [1, 2, 3, 4, 5];
 expected = [10, 14];
 @test rolling(F, D, W) == expected
 
-D₁ = [1, 2, 3, 4, 5]
-D₂ = [5, 4, 3, 2, 1]
-F = cor
-W = 3
-expected = [-1.0, -1.0, -1.0]
+D₁ = [1, 2, 3, 4, 5];
+D₂ = [5, 4, 3, 2, 1];
+F = cor;
+W = 3;
+expected = [-1.0, -1.0, -1.0];
 @test rolling(F, D₁, D₂, W) == expected
 
 expected = [missing, missing, -1.0, -1.0, -1.0];
@@ -41,15 +41,17 @@ expected = [-1.0, -1.0, -1.0, missing, missing];
 @test map(clean, rolling(F, D₁, D₂, W; padding=missing, padlast=true)) == map(clean, expected)
 @test typeof(rolling(F, D₁, D₂, W; padding=missing, padlast=true)) == typeof(expected)
 
-D₁ = [1, 2, 3, 4, 5]
-D₂ = [5, 4, 3, 2, 1]
-M = hcat(D₁, D₂)
-F = sum
-W = 3
+D₁ = [1, 2, 3, 4, 5];
+D₂ = [5, 4, 3, 2, 1];
+M = hcat(D₁, D₂);
+F = sum;
+W = 3;
+
 expected = [
     6 12
     9 9
-    12 6]
+    12 6];
+
 @test rolling(F, M, W) == expected
 @test typeof(rolling(F, M, W)) == typeof(expected)
 
@@ -58,7 +60,8 @@ expected = [
     missing missing
     6 12
     9 9
-    12 6]
+    12 6];
+
 @test map(clean, rolling(F, M, W; padding=missing)) == map(clean, expected)
 @test typeof(rolling(F, M, W; padding=missing)) == typeof(expected)
 
@@ -67,7 +70,8 @@ expected = [
     9 9
     12 6
     missing missing
-    missing missing]
+    missing missing];
+
 @test map(clean, rolling(F, M, W; padding=missing, padlast=true)) == map(clean, expected)
 @test typeof(rolling(F, M, W; padding=missing, padlast=true)) == typeof(expected)
 
