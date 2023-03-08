a8c8f2170f0e920461c642f8b1199a2b7e2dc8fd
Jeffrey Sarnoff <jeffrey.sarnoff@gmail.com>
2023-03-08 01:16:42 -0500

    NoPadding

--- src/support/types.jl
+++ src/support/types.jl
@@ -2,3 +2,6 @@ const Seq = Union{V, NT} where {N, T, V<:AbstractVector{T}, NT<:NTuple{N,T}}
 
 seq(x::AbstractVector{T}) where {T} = x
 seq(x::NTuple{N,T}) where {N,T} = x
+
+struct NoPadding end
+const nopadding = NoPadding()
