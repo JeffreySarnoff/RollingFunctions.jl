3736672313e119ba686dae885561a26044821b1f
Jeffrey Sarnoff <jeffrey.sarnoff@gmail.com>
2023-04-09 08:19:50 -0400

    update

--- test/matrix_rolling.jl
+++ test/matrix_rolling.jl
@@ -5,7 +5,7 @@ D₁ = [1, 2, 3, 4, 5];
 D₂ = [5, 4, 3, 2, 1];
 M = hcat(D₁, D₂);
 F = sum;
-W = 3;
+S = 3;
 
 wweights = ProbabilityWeights([0.1,0.2,0.7])
 mweights = hcat(wweights, wweights)
@@ -15,8 +15,8 @@ expected = [
     9 9
     12 6];
 
-@test rolling(F, W, M) == expected
-@test typeof(rolling(F, W, M)) == typeof(expected)
+@test rolling(F, S, M) == expected
+@test typeof(rolling(F, S, M)) == typeof(expected)
 
 expected = [
     missing missing
@@ -25,8 +25,8 @@ expected = [
     9 9
     12 6];
 
-@test map(clean, rolling(F, W, M; padding=missing)) == map(clean, expected)
-@test typeof(rolling(F, W, M; padding=missing)) == typeof(expected)
+@test map(clean, rolling(F, S, M; padding=missing)) == map(clean, expected)
+@test typeof(rolling(F, S, M; padding=missing)) == typeof(expected)
 
 expected = [
     6 12
@@ -35,8 +35,8 @@ expected = [
     missing missing
     missing missing];
 
-@test map(clean, rolling(F, W, M; padding=missing, padlast=true)) == map(clean, expected)
-@test typeof(rolling(F, W, M; padding=missing, padlast=true)) == typeof(expected)
+@test map(clean, rolling(F, S, M; padding=missing, padlast=true)) == map(clean, expected)
+@test typeof(rolling(F, S, M; padding=missing, padlast=true)) == typeof(expected)
 
 
 D₁ = [1, 2, 3, 4, 5]
@@ -44,11 +44,11 @@ D₂ = [5, 4, 3, 2, 1]
 M = hcat(D₁, D₂)
 M2 = Float64.(M)
 F = sum
-W = 3
+S = 3
 expected = [  2.6 3.4
               3.6 2.4
               4.6 1.4 ]
-@test isapprox(rolling(F, W, M2, wweights), expected)
+@test isapprox(rolling(F, S, M2, wweights), expected)
 
-@test isapprox(rolling(F, W, M, wweights), expected
+@test isapprox(rolling(F, S, M, wweights), expected
 
