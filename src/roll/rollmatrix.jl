081d7a0be85b935fd577965e28137531c92943bd
Jeffrey Sarnoff <jeffrey.sarnoff@gmail.com>
2023-04-08 22:42:58 -0400

    update rollmatrix

--- src/roll/rollmatrix.jl
+++ src/roll/rollmatrix.jl
@@ -1,24 +1,24 @@
 #=
-     basic_rolling(window_fn::Function, ::Matrix, window_span)
-     padded_rolling(window_fn::Function, ::Matrix, window_span; padding, padlast)
-     last_padded_rolling(window_fn::Function, ::Matrix, window_span; padding, padlast)
+     basic_rolling(func::Function, ::Matrix, span)
+     padfirst_rolling(func::Function, ::Matrix, span; padding, padlast)
+     padfinal_rolling(func::Function, ::Matrix, span; padding, padlast)
 
-     basic_rolling(window_fn::Function, ::Matrix, window_span, weights)
-     padded_rolling(window_fn::Function, ::Matrix, window_span, weights; padding, padlast)
-     last_padded_rolling(window_fn::Function, ::Matrix, window_span, weights; padding, padlast)
+     basic_rolling(func::Function, ::Matrix, span, weights)
+     padfirst_rolling(func::Function, ::Matrix, span, weights; padding, padlast)
+     padfinal_rolling(func::Function, ::Matrix, span, weights; padding, padlast)
 =#
 
-function basic_rolling(window_fn::Function, data::AbstractMatrix{T}, window_span::Span) where {T}
+function basic_rolling(func::Function, span::Span, data::AbstractMatrix{T}) where {T}
     ᵛʷdata = asview(data)
     n = nrows(ᵛʷdata)
-    nvalues  = nrolled(n, window_span) 
+    nvalues  = nrolled(n, span) 
     # there are 1 or more columns, each holds `n` values
-    rettype  = rts(window_fn, (T,))
+    rettype  = rts(func, (T,))
     results = Matrix{rettype}(undef, (nvalues, ncols(ᵛʷdata)))
 
-    ilow, ihigh = 1, window_span
+    ilow, ihigh = 1, span
     @inbounds for idx in eachindex(eachrow(results))
-        @views results[idx, :] .= map(window_fn, eachcol(ᵛʷdata[ilow:ihigh, :]))
+        @views results[idx, :] .= map(func, eachcol(ᵛʷdata[ilow:ihigh, :]))
         ilow = ilow + 1
         ihigh = ihigh + 1
     end
@@ -28,26 +28,26 @@ end
 
 # pad the dropped indicies with a given padding value
 
-function padded_rolling(window_fn::Function, data::AbstractMatrix{T}, window_span::Span;
+function padfirst_rolling(func::Function, span::Span, data::AbstractMatrix{T};
                         padding=nopadding) where {T}
     ᵛʷdata = asview(data)
     n = nrows(ᵛʷdata)
-    nvalues  = nrolled(n, window_span) 
-    rettype  = Union{typeof(padding), rts(window_fn, (T,))}
+    nvalues  = nrolled(n, span) 
+    rettype  = Union{typeof(padding), rts(func, (T,))}
     results = Matrix{rettype}(undef, (nvalues, ncols(ᵛʷdata)))
     
-    # only completed window_span coverings are resolvable
-    # the first (window_span - 1) values are unresolved wrt window_fn
+    # only completed span coverings are resolvable
+    # the first (span - 1) values are unresolved wrt func
     # this is the padding_span
-    padding_span = window_span - 1  
+    padding_span = span - 1  
     padding_idxs = 1:padding_span
 
     results = Matrix{Union{typeof(padding), rettype}}(undef, size(ᵛʷdata))  
     results[padding_idxs, :] .= padding
 
-    ilow, ihigh = 1, window_span
-    @inbounds for idx in window_span:n 
-        @views results[idx, :] .= map(window_fn, eachcol(ᵛʷdata[ilow:ihigh, :]))
+    ilow, ihigh = 1, span
+    @inbounds for idx in span:n 
+        @views results[idx, :] .= map(func, eachcol(ᵛʷdata[ilow:ihigh, :]))
         ilow = ilow + 1
         ihigh = ihigh + 1
     end
@@ -57,25 +57,25 @@ end
 
 # pad the last entries, move windowed data back to the first entries
 
-function last_padded_rolling(window_fn::Function, data::AbstractMatrix{T}, window_span::Span;
+function padfinal_rolling(func::Function, span::Span, data::AbstractMatrix{T};
                              padding=nopadding) where {T}
     ᵛʷdata = asview(data)
     n = nrows(ᵛʷdata)
-    nvalues  = nrolled(n, window_span) 
-    rettype  = Union{typeof(padding), rts(window_fn, (T,))}
+    nvalues  = nrolled(n, span) 
+    rettype  = Union{typeof(padding), rts(func, (T,))}
     
-    # only completed window_span coverings are resolvable
-    # the first (window_span - 1) values are unresolved wrt window_fn
+    # only completed span coverings are resolvable
+    # the first (span - 1) values are unresolved wrt func
     # this is the padding_span
-    padding_span = window_span - 1  
+    padding_span = span - 1  
     padding_idxs = n-padding_span:n
 
     results = Matrix{rettype}(undef, size(ᵛʷdata))
     results[padding_idxs, :] .= padding
     
-    ilow, ihigh = 1, window_span
+    ilow, ihigh = 1, span
     @inbounds for idx in 1:n-padding_span
-        @views results[idx, :] = map(window_fn, eachcol(ᵛʷdata[ilow:ihigh,:]))
+        @views results[idx, :] = map(func, eachcol(ᵛʷdata[ilow:ihigh,:]))
         ilow = ilow + 1
         ihigh = ihigh + 1
     end
@@ -85,17 +85,17 @@ end
 
 # weighted
 
-function basic_rolling(window_fn::Function, data::AbstractMatrix{T}, window_span::Span, weights::AbstractVector{T}) where {T}
+function basic_rolling(func::Function, span::Span, data::AbstractMatrix{T}, weights::AbstractVector{T}) where {T}
     ᵛʷdata = asview(data)
     n = nrows(ᵛʷdata)
-    nvalues  = nrolled(n, window_span) 
+    nvalues  = nrolled(n, span) 
     # there are 1 or more columns, each holds `n` values
-    rettype  = rts(window_fn, (T,))
+    rettype  = rts(func, (T,))
     results = Matrix{rettype}(undef, (nvalues, ncols(ᵛʷdata)))
 
-    ilow, ihigh = 1, window_span
+    ilow, ihigh = 1, span
     @inbounds for idx in eachindex(eachrow(results))
-        @views results[idx, :] .= map(window_fn, eachcol(ᵛʷdata[ilow:ihigh, :] .* weights))
+        @views results[idx, :] .= map(func, eachcol(ᵛʷdata[ilow:ihigh, :] .* weights))
         ilow = ilow + 1
         ihigh = ihigh + 1
     end
@@ -105,26 +105,26 @@ end
 
 # pad the dropped indicies with a given padding value
 
-function padded_rolling(window_fn::Function, data::AbstractMatrix{T}, window_span::Span, weights::AbstractVector{T};
+function padfirst_rolling(func::Function, span::Span, data::AbstractMatrix{T}, weights::AbstractVector{T};
                         padding=nopadding) where {T}
     ᵛʷdata = asview(data)
     n = nrows(ᵛʷdata)
-    nvalues  = nrolled(n, window_span) 
-    rettype  = Union{typeof(padding), rts(window_fn, (T,))}
+    nvalues  = nrolled(n, span) 
+    rettype  = Union{typeof(padding), rts(func, (T,))}
     results = Matrix{rettype}(undef, (nvalues, ncols(ᵛʷdata)))
     
-    # only completed window_span coverings are resolvable
-    # the first (window_span - 1) values are unresolved wrt window_fn
+    # only completed span coverings are resolvable
+    # the first (span - 1) values are unresolved wrt func
     # this is the padding_span
-    padding_span = window_span - 1  
+    padding_span = span - 1  
     padding_idxs = 1:padding_span
 
     results = Matrix{Union{typeof(padding), rettype}}(undef, size(ᵛʷdata))  
     results[padding_idxs, :] .= padding
 
-    ilow, ihigh = 1, window_span
-    @inbounds for idx in window_span:n 
-        @views results[idx, :] .= map(window_fn, eachcol(ᵛʷdata[ilow:ihigh, :] .* weights))
+    ilow, ihigh = 1, span
+    @inbounds for idx in span:n 
+        @views results[idx, :] .= map(func, eachcol(ᵛʷdata[ilow:ihigh, :] .* weights))
         ilow = ilow + 1
         ihigh = ihigh + 1
     end
@@ -134,25 +134,25 @@ end
 
 # pad the last entries, move windowed data back to the first entries
 
-function last_padded_rolling(window_fn::Function, data::AbstractMatrix{T}, window_span::Span, weights::AbstractVector{T};
+function padfinal_rolling(func::Function, span::Span, data::AbstractMatrix{T}, weights::AbstractVector{T};
                              padding=nopadding) where {T}
     ᵛʷdata = asview(data)
     n = nrows(ᵛʷdata)
-    nvalues  = nrolled(n, window_span) 
-    rettype  = Union{typeof(padding), rts(window_fn, (T,))}
+    nvalues  = nrolled(n, span) 
+    rettype  = Union{typeof(padding), rts(func, (T,))}
     
-    # only completed window_span coverings are resolvable
-    # the first (window_span - 1) values are unresolved wrt window_fn
+    # only completed span coverings are resolvable
+    # the first (span - 1) values are unresolved wrt func
     # this is the padding_span
-    padding_span = window_span - 1  
+    padding_span = span - 1  
     padding_idxs = n-padding_span:n
 
     results = Matrix{rettype}(undef, size(ᵛʷdata))
     results[padding_idxs, :] .= padding
     
-    ilow, ihigh = 1, window_span
+    ilow, ihigh = 1, span
     @inbounds for idx in 1:n-padding_span
-        @views results[idx, :] = map(window_fn, eachcol(ᵛʷdata[ilow:ihigh,:] .* weights))
+        @views results[idx, :] = map(func, eachcol(ᵛʷdata[ilow:ihigh,:] .* weights))
         ilow = ilow + 1
         ihigh = ihigh + 1
     end
