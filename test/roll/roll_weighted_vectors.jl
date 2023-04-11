03210faf48600cbb234d457065e721bae6ced3b4
Jeffrey Sarnoff <jeffrey.sarnoff@gmail.com>
2023-04-10 22:28:00 -0400

    refactor

--- test/roll/roll_weighted_vectors.jl
+++ test/roll/roll_weighted_vectors.jl
@@ -1,8 +1,11 @@
 clean(x) = x
 clean(x::Missing) = Missing
 
-F = sum; W = 3; weights = ProbabilityWeights([0.2, 0.3, 0.5])
+F = sum;
+W = 3;
+weights = ProbabilityWeights([0.2, 0.3, 0.5]);
 D = [1, 2, 3, 4, 5];
+
 expected = [2.3, 3.3, 4.3];
 @test rolling(F, W, D, weights) == expected
 
@@ -13,4 +16,15 @@ expected = [missing, missing, 2.3, 3.3, 4.3];
 expected = [2.3, 3.3, 4.3, missing, missing];
 @test map(clean, rolling(F, W, D, weights; padding=missing, padlast=true)) == map(clean, expected)
 @test typeof(rolling(F, W, D, weights; padding=missing, padlast=true)) == typeof(expected)
+
+
+
+D₁ = [1, 2, 3, 4, 5]
+D₂ = [5, 4, 3, 2, 1]
+F = cor
+W = 3
+weights = ProbabilityWeights([0.2, 0.3, 0.5]);
+
+expected = [0.9946433500242822, 0.9773555548504419, -0.9511012772444227]
+@test rolling(F, W, D₁, D₂, weights) == expected
 
