11fb7faa984e586b12f62f17a2d24d8cc20873fe
Jeffrey Sarnoff <jeffrey.sarnoff@gmail.com>
2023-04-15 04:16:36 -0400

    fix

--- test/roll/roll_stats.jl
+++ test/roll/roll_stats.jl
@@ -28,7 +28,7 @@ expected = [0.9751641759537001, 0.9283031155706918, 0.9749534026403799, 0.973216
 obtained = rollcor(4, data1, data2, weighting)
 @test eps(Float32) > abs(sum(expected .- obtained))
 
-expected = [0.0, 1.5, 2.5, 7.833333333333333, 15.0, 20.75, 26.5]
-obtained = runcov(5, data1, data2)
+expected = [2.5, 7.833333333333333, 15.0, 20.75, 26.5]
+obtained = rollcov(5, data1, data2)
 @test eps(Float32) > abs(sum(expected .- obtained))
 
