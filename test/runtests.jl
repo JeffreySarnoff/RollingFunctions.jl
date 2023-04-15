034ab9ee8a47ab9e09226be05bd589cb9a0b3149
Jeffrey Sarnoff <jeffrey.sarnoff@gmail.com>
2023-04-15 03:52:33 -0400

    test

--- test/runtests.jl
+++ test/runtests.jl
@@ -8,7 +8,7 @@ include("roll/roll.jl")
 include("roll/roll_vectors.jl")
 include("roll/roll_weighted_vectors.jl")
 include("roll/roll_matrix.jl")
-#include("roll/roll_stats.jl")
+include("roll/roll_stats.jl")
 
 #=
 include("tile/tile.jl")
