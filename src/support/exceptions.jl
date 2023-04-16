f71c9101dbf43c4d8693d45117930aa5b40c4863
Jeffrey Sarnoff <jeffrey.sarnoff@gmail.com>
2023-04-16 03:19:58 -0400

    exceptions

--- src/support/exceptions.jl
+++ src/support/exceptions.jl
@@ -25,29 +25,29 @@ end
 check_empty(sequence) = 
     isempty(sequence) && empty_error(sequence)
 
-check_span(seqlength, windowspan) =
+check_span(seqlength::Int, windowspan::Span) =
     ((windowspan > seqlength) || (iszero(seqlength))) && span_error(seqlength, windowspan)
 
-check_trimspan(seqlength, windowspan) =
+check_trimspan(seqlength::Int, windowspan::Span) =
     windowspan > (seqlength - windowspan + 1) && trimspan_error(seqlength, windowspan)
 
-check_tile(seqlength, tilespan) =
+check_tile(seqlength::Int, tilespan::Span) =
     ((tilespan > seqlength) || (iszero(seqlength))) && tile_error(seqlength, tilespan)
 
-check_tilespan(seqlength, tilespan) =
+check_tilespan(seqlength::Int, tilespan::Span) =
     tilespan > (seqlength - windowspan + 1) && tilespan_error(seqlength, tilespan)
 
-check_weights(nweights, windowspan) =
+check_weights(nweights::Int, windowspan::Span) =
     (nweights == windowspan) || weights_error(nweights, windowspan)
 
-check_weights(nweights1, nweights2, windowspan) =
+check_weights(nweights1::Int, nweights2::Int, windowspan::Span) =
     (nweights1 === nweights2 == windowspan) || weights_error(length(weights1), windowspan)
 
-check_weights(nweights1, nweights2, nweights3, windowspan) =
+check_weights(nweights1::Int, nweights2::Int, nweights3::Int, windowspan::Span) =
     (nweights1 === nweights2 === nweights3 == windowspan) || weights_error(length(weights1), windowspan)
 
-check_weights(windowspan, nweights::Tuple) =
-    (windowspan .== nweights) || weights_error("length.(weights) must equal windowspan")
+check_weights(nweights::NTuple{N,Int}, windowspan::Span) where {N} =
+    (all(windowspan .== nweights)) || weights_error("all length.(weights) must equal windowspan")
 
 
 function empty_error(sequence)
@@ -56,31 +56,31 @@ function empty_error(sequence)
     throw(err)
 end
 
-function span_error(seqlength, windowspan)
+function span_error(seqlength::Int, windowspan::Span)
     str = string("Bad window span (", windowspan, ") for length (", seqlength, ").")
     err = SpanError(str)
     throw(err)
 end
 
-function trimspan_error(seqlength, windowspan)
+function trimspan_error(seqlength::Int, windowspan::Span)
     str = string("Bad window span (", windowspan, ") for trimmed length (", seqlength, ").")
     err = SpanError(str)
     throw(err)
 end
 
-function tile_error(seqlength, tilespan)
+function tile_error(seqlength::Int, tilespan::Span)
     str = string("Bad tile span (", tilespan, ") for length (", seqlength, ").")
     err = TileError(str)
     throw(err)
 end
 
-function trimtile_error(seqlength, tilespan)
+function trimtile_error(seqlength::Int, tilespan::Span)
     str = string("Bad tile span (", tilespan, ") for trimmed length (", seqlength, ").")
     err = TileError(str)
     throw(err)
 end
 
-function weights_error(nweighting, windowspan)
+function weights_error(nweighting::Int, windowspan::Span)
     str = string("Bad weights length (", nweighting, ") != for window length (", windowspan, ").")
     err = WeightsError(str)
     throw(err)
