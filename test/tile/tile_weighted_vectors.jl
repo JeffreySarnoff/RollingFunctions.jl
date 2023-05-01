353365eb5ff80243b920131661b0af199cb7bb78
Jeffrey Sarnoff <jeffrey.sarnoff@gmail.com>
2023-05-01 06:03:57 -0400

    tiling tests

--- test/tile/tile_weighted_vectors.jl
+++ test/tile/tile_weighted_vectors.jl
@@ -3,14 +3,14 @@ clean(x::Missing) = Missing
 
 F = sum; W = 3; weights = ProbabilityWeights([0.2, 0.3, 0.5])
 D = [1, 2, 3, 4, 5];
-expected = [2.3, 3.3, 4.3];
+expected = [2.3];
 @test tiling(F, W, D, weights) == expected
 
-expected = [missing, missing, 2.3, 3.3, 4.3];
+expected = [missing, 2.3];
 @test map(clean, tiling(F, W, D, weights; padding=missing)) == map(clean, expected)
 @test typeof(tiling(F, W, D, weights; padding=missing)) == typeof(expected)
 
-expected = [2.3, 3.3, 4.3, missing, missing];
+expected = [2.3, missing];
 @test map(clean, tiling(F, W, D, weights; padding=missing, padlast=true)) == map(clean, expected)
 @test typeof(tiling(F, W, D, weights; padding=missing, padlast=true)) == typeof(expected)
 
