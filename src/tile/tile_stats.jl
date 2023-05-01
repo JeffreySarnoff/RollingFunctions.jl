3c0404094d4b808b9a90fd73dc99ba5bff78666e
Jeffrey Sarnoff <jeffrey.sarnoff@gmail.com>
2023-05-01 01:27:04 -0400

    reset

--- src/tile/tile_stats.jl
+++ src/tile/tile_stats.jl
@@ -3,9 +3,9 @@ for T1 in (:T, :(Union{Missing,T}))
         (:tilemean, :vmean), (:tilesum, :vsum),
         (:tilevar, :vvar), (:tilestd, :vstd))
         @eval begin
-            $R(width::Width, data::V; padding=nopadding, padlast=false) where {T, V<:AbstractVector{$T1}} =
+            $R(width::Span, data::V; padding=nopadding, padlast=false) where {T, V<:AbstractVector{$T1}} =
                 tiling($F, width, data; padding, padlast)
-            $R(width::Width, data::V, weights::W; padding=nopadding, padlast=false) where {T, V<:AbstractVector{$T1}, W<:AbstractWeights} =
+            $R(width::Span, data::V, weights::W; padding=nopadding, padlast=false) where {T, V<:AbstractVector{$T1}, W<:AbstractWeights} =
                 tiling($F, width, data, weights; padding, padlast)
         end
     end
@@ -14,11 +14,11 @@ end
 for T1 in (:T, :(Union{Missing,T}))
     for (R, F) in ((:tilecor, :vcor), (:tilecov, :vcov))
         @eval begin
-            $R(width::Width, data1::V, data2::V; padding=nopadding, padlast=false) where {T,V<:AbstractVector{$T1}} =
+            $R(width::Span, data1::V, data2::V; padding=nopadding, padlast=false) where {T,V<:AbstractVector{$T1}} =
                 tiling($F, width, data1, data2; padding, padlast)
-            $R(width::Width, data1::V, data2::V, weights::W; padding=nopadding, padlast=false) where {T,V<:AbstractVector{$T1},W<:AbstractWeights} =
+            $R(width::Span, data1::V, data2::V, weights::W; padding=nopadding, padlast=false) where {T,V<:AbstractVector{$T1},W<:AbstractWeights} =
                 tiling($F, width, data1, data2, weights, weights; padding, padlast)
-            $R(width::Width, data1::V, data2::V, weights1::W, weights2::W; padding=nopadding, padlast=false) where {T,V<:AbstractVector{$T1},W<:AbstractWeights} =
+            $R(width::Span, data1::V, data2::V, weights1::W, weights2::W; padding=nopadding, padlast=false) where {T,V<:AbstractVector{$T1},W<:AbstractWeights} =
                 tiling($F, width, data1, data2, weights1, weights2; padding, padlast)
         end
     end
