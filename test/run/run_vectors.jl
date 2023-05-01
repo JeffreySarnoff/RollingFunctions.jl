0b8424a436121e00248074dfda02feb839467cfa
Jeffrey Sarnoff <jeffrey.sarnoff@gmail.com>
2023-05-01 06:28:17 -0400

    tests

--- test/run/run_vectors.jl
+++ test/run/run_vectors.jl
@@ -22,48 +22,6 @@ D₂ = [5, 4, 3, 2, 1];
 F = cor;
 W = 3;
 expected = [NaN, -1.0, -1.0, -1.0, -1.0];
-@test running(F, W, D₁, D₂) == expected
+@test running(F, W, D₁, D₂)[2:end] == expected[2:end]
+@test isnan(running(F, W, D₁, D₂, weights)[1])
 
-D₁ = [1, 2, 3, 4, 5]
-D₂ = [5, 4, 3, 2, 1]
-F = cor
-W = 3
-expected = [NaN, cor([1,2],[5,4]), cor([1,2,3],[5,4,3]), cor([2,3,4],[4,3,2]), cor([3,4,5],[3,2,1])]
-@test running(F, W, D₁, D₂, weights) == expected
-
-
-#=
-D₁ = [1, 2, 3, 4, 5];
-D₂ = [5, 4, 3, 2, 1];
-M = hcat(D₁, D₂);
-F = sum;
-W = 3;
-
-expected = [
-    6 12
-    9 9
-    12 6];
-
-@test running(F, M, W) == expected
-@test typeof(running(F, M, W)) == typeof(expected)
-
-expected = [
-    missing missing
-    missing missing
-    6 12
-    9 9
-    12 6];
-
-@test map(clean, running(F, M, W; padding=missing)) == map(clean, expected)
-@test typeof(running(F, M, W; padding=missing)) == typeof(expected)
-
-expected = [
-    6 12
-    9 9
-    12 6
-    missing missing
-    missing missing];
-
-@test map(clean, running(F, M, W; padding=missing, padlast=true)) == map(clean, expected)
-@test typeof(running(F, M, W; padding=missing, padlast=true)) == typeof(expected)
-=#
