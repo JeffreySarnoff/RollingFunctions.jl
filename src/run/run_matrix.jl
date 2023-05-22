680aaaa28f11f1f3d79f05899046a276f3b63207
Jeffrey Sarnoff <jeffrey.sarnoff@gmail.com>
2023-05-22 00:02:03 -0400

    running

--- src/run/run_matrix.jl
+++ src/run/run_matrix.jl
@@ -6,9 +6,62 @@
      taperfinal(func::F, width, ::Matrix, weight)
 =#
 
+function taperfirst(func::F, width::Width, data::AbstractMatrix{T}) where {T,F<:Function}
+    ᵛʷdata = asview(data)
+    n = nrows(ᵛʷdata)
+    rettype = rts(func, (T,))
+    results = Matrix{rettype}(undef, (n, ncols(ᵛʷdata)))
+
+    # only completed width coverings are fully resolvable
+    # the first (width - 1) values are to be tapered
+    taper_idxs = 1:width-1
+
+    results = Matrix{rettype}(undef, size(ᵛʷdata))
+
+    ilow = 1
+    @inbounds for idx in taper_idxs
+       @views results[idx, :] .= map(func, eachcol(ᵛʷdata[ilow:idx, :]))
+    end
+
+    ilow, ihigh = 1, width
+    @inbounds for idx in width:n
+        @views results[idx, :] .= map(func, eachcol(ᵛʷdata[ilow:ihigh, :]))
+        ilow = ilow + 1
+        ihigh = ihigh + 1
+    end
+
+    results
+end
+
+function taperfinal(func::F, width::Width, data::AbstractMatrix{T}) where {T, F<:Function}
+    ᵛʷdata = asview(data)
+    n = nrows(ᵛʷdata)
+    rettype = rts(func, (T,))
+
+    # only completed width coverings are fully resolvable
+    # the last (width - 1) values are to be tapered
+    taper_idxs = n-width+2:n
+
+    results = Matrix{rettype}(undef, size(ᵛʷdata))
+
+    @inbounds for idx in taper_idxs
+        @views results[idx, :] .= map(func, eachcol(ᵛʷdata[idx:n, :]))
+    end
+
+    ilow, ihigh = 1, width
+    @inbounds for idx in 1:n-width+1
+        @views results[idx, :] = map(func, eachcol(ᵛʷdata[ilow:ihigh, :]))
+        ilow = ilow + 1
+        ihigh = ihigh + 1
+    end
+
+   results
+end
+
 
 # taper the dropped indicies with a given tapering value
 
+#=
 function taperfirst(func::Function, width::Width, data::AbstractMatrix{T}) where {T}
     ᵛʷdata = asview(data)
     n = nrows(ᵛʷdata)
