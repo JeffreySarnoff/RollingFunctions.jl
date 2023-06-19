ea6c3846a27ef77944775e7093268e71cec18def
Jeffrey Sarnoff <jeffrey.sarnoff@gmail.com>
2023-06-18 23:31:36 -0400

    test all stats

--- test/run/run_stats.jl
+++ test/run/run_stats.jl
@@ -21,12 +21,10 @@ expected = [NaN, 1.5, 2.5, 6.0, 9.5, 10.0, 12.0]
 obtained = running(cov, 3, data1, data2)
 @test isapprox(filter(!isnan,obtained), filter(!isnan,expected))
 
-weighting = AnalyticWeights(normalize([1.0f0, 2.0f0, 4.0f0))
+weighting = AnalyticWeights(normalize([1.0f0, 2.0f0, 4.0f0]))
 
-expected = Float32[NaN, 1.5, 2.5, 7.83333, 12.1667, 16.5, 18.3333]
+expected = Float32[0.999406, 0.987553, 0.999818, 0.0, 0.0, 0.0, 0.0]
 obtained = map(Float32, runcor(windowsize, data1, data2, weighting))
 @test isapprox(obtained, expected)
-
-expected = [15.0, 20.75, 26.5]
-obtained = runcov(5, data1, data2, weighting ,weighting)
+obtained = map(Float32, runcor(windowsize, data1, data2, weighting, weighting))
 @test isapprox(obtained, expected)
