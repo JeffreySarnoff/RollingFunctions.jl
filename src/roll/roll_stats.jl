2cc229d831ab30a81743971e7caef46ff1c6c922
Jeffrey Sarnoff <jeffrey.sarnoff@gmail.com>
2023-06-18 23:54:26 -0400

    v 0.9.75

--- src/roll/roll_stats.jl
+++ src/roll/roll_stats.jl
@@ -4,19 +4,19 @@ for (R, F) in ((:rollmin, :vminimum), (:rollmax, :vmaximum),
     (:rollvar, :vvar), (:rollstd, :vstd))
     @eval begin
         $R(width::Integer, data; padding=nopadding, atend=false) =
-            rolling($F, width, data; padding, atend)
+            rolling(VectorizedStatistics.$F, width, data; padding, atend)
         $R(width::Integer, data, weights; padding=nopadding, atend=false) =
-            rolling($F, width, data, weights; padding, atend)
+            rolling(VectorizedStatistics.$F, width, data, weights; padding, atend)
     end
 end
 
 for (R, F) in ((:rollcor, :vcor), (:rollcov, :vcov))
     @eval begin
         $R(width::Integer, data1, data2; padding=nopadding, atend=false) =
-            rolling($F, width, data1, data2; padding, atend)
+            rolling(VectorizedStatistics.$F, width, data1, data2; padding, atend)
         $R(width::Integer, data1, data2, weights; padding=nopadding, atend=false) =
-            rolling($F, width, data1, data2, weights, weights; padding, atend)
+            rolling(VectorizedStatistics.$F, width, data1, data2, weights, weights; padding, atend)
         $R(width::Integer, data1, data2, weights1, weights2; padding=nopadding, atend=false) =
-            rolling($F, width, data1, data2, weights1, weights2; padding, atend)
+            rolling(VectorizedStatistics.$F, width, data1, data2, weights1, weights2; padding, atend)
     end
 end
--- src/tile/tile_stats.jl
+++ src/tile/tile_stats.jl
@@ -4,19 +4,19 @@ for (R, F) in ((:tilemin, :vminimum), (:tilemax, :vmaximum),
     (:tilevar, :vvar), (:tilestd, :vstd))
     @eval begin
         $R(width::Integer, data; padding=nopadding, atend=false) =
-            tiling($F, width, data; padding, atend)
+            tiling(VectorizedStatistics.$F, width, data; padding, atend)
         $R(width::Integer, data, weights; padding=nopadding, atend=false) =
-            tiling($F, width, data, weights; padding, atend)
+            tiling(VectorizedStatistics.$F, width, data, weights; padding, atend)
     end
 end
 
 for (R, F) in ((:tilecor, :vcor), (:tilecov, :vcov))
     @eval begin
         $R(width::Integer, data1, data2; padding=nopadding, atend=false) =
-            tiling($F, width, data1, data2; padding, atend)
+            tiling(VectorizedStatistics.$F, width, data1, data2; padding, atend)
         $R(width::Integer, data1, data2, weights; padding=nopadding, atend=false) =
-            tiling($F, width, data1, data2, weights, weights; padding, atend)
+            tiling(VectorizedStatistics.$F, width, data1, data2, weights, weights; padding, atend)
         $R(width::Integer, data1, data2, weights1, weights2; padding=nopadding, atend=false) =
-            tiling($F, width, data1, data2, weights1, weights2; padding, atend)
+            tiling(VectorizedStatistics.$F, width, data1, data2, weights1, weights2; padding, atend)
     end
 end
