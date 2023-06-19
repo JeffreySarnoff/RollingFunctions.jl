4052cb41c64f4d6cae300c79bb6e0d5359dbe30f
Jeffrey Sarnoff <jeffrey.sarnoff@gmail.com>
2023-06-18 23:52:17 -0400

    explicit VectorizedStatistics

--- src/run/run_stats.jl
+++ src/run/run_stats.jl
@@ -4,19 +4,19 @@ for (R, F) in ( (:runmin, :vminimum), (:runmax, :vmaximum),
                 (:runvar, :vvar), (:runstd, :vstd) )
     @eval begin
         $R(width::Integer, data) =
-            running($F, width, data)
+            running(VectorizedStatistics.$F, width, data)
         $R(width::Integer, data, weights) =
-            running($F, width, data, weights)
+            running(VectorizedStatistics.$F, width, data, weights)
     end
 end
 
 for (R, F) in ((:runcor, :vcor), (:runcov, :vcov))
     @eval begin
         $R(width::Integer, data1, data2) =
-            running($F, width, data1, data2)
+            running(VectorizedStatistics.$F, width, data1, data2)
         $R(width::Integer, data1, data2, weights) =
-            running($F, width, data1, data2, weights, weights)
+            running(VectorizedStatistics.$F, width, data1, data2, weights, weights)
         $R(width::Integer, data1, data2, weights1, weights2) =
-            running($F, width, data1, data2, weights1, weights2)
+            running(VectorizedStatistics.$F, width, data1, data2, weights1, weights2)
     end
 end
--- test/run/run_stats.jl
+++ test/run/run_stats.jl
@@ -1,3 +1,8 @@
+datavec = collect(1.0f0:5.0f0)
+data1 = Float32[1, 2, 3, 4, 5, 6, 7]
+data2 = Float32[1, 4, 6, 16, 25, 36, 49]
+weighting = AnalyticWeights(normalize([1.0f0, 2.0f0, 4.0f0]))
+windowsize = 3
 
 expected = Float32[1.0, 1.5, 2.0, 3.0, 4.0]
 @test isapprox(runmean(windowsize, datavec), expected)
@@ -15,8 +20,7 @@ expected = [NaN, 1.5, 2.5, 6.0, 9.5, 10.0, 12.0]
 obtained = running(cov, 3, data1, data2)
 @test isapprox(filter(!isnan,obtained), filter(!isnan,expected))
 
-expected = Float32[3.19048, 11.0476, 20.1905, 0.0, 0.0, 0.0, 0.0]
-obtained = map(Float32, runcov(windowsize, data1, data2, weighting))
+expected = Float32[0.999406, 0.987553, 0.999818, 0.0, 0.0, 0.0, 0.0]
+obtained = map(Float32, runcor(windowsize, data1, data2, weighting))
 @test isapprox(obtained, expected)
-obtained = map(Float32, runcov(windowsize, data1, data2, weighting, weighting))
-@test isapprox(obtained, expected)
+obtained = map(Float32, runcor(windowsize, data1, data2, weighting, weighting))
