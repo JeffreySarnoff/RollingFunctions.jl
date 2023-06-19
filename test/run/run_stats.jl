983b88936c116bd68650efaf52d94061343bc453
Jeffrey Sarnoff <jeffrey.sarnoff@gmail.com>
2023-06-19 09:40:13 -0400

    fix test

--- test/run/run_stats.jl
+++ test/run/run_stats.jl
@@ -20,7 +20,7 @@ expected = [NaN, 1.5, 2.5, 6.0, 9.5, 10.0, 12.0]
 obtained = running(cov, 3, data1, data2)
 @test isapprox(filter(!isnan,obtained), filter(!isnan,expected))
 
-expected = Float32[0.999406, 0.987553, 0.999818, 0.0, 0.0, 0.0, 0.0]
+expected = Float32[3.1904764, 11.047618, 20.190474, 0.0, 0.0, 0.0, 0.0]
 obtained = map(Float32, runcov(windowsize, data1, data2, weighting))
 @test isapprox(obtained, expected)
 obtained = map(Float32, runcov(windowsize, data1, data2, weighting, weighting))
