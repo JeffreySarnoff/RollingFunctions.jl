c486a8eff85362bbe37e83db28ef44f259287faf
Jeffrey Sarnoff <jeffrey.sarnoff@gmail.com>
2023-07-01 20:16:52 -0400

    restore runstats for cor, cov with padding for first value

--- src/run/run_stats.jl
+++ src/run/run_stats.jl
@@ -10,15 +10,28 @@ for (R, F) in ( (:runmin, :vminimum), (:runmax, :vmaximum),
     end
 end
 
-#=
 for (R, F) in ((:runcor, :vcor), (:runcov, :vcov))
     @eval begin
-        $R(width::Integer, data1, data2) =
-            running(VectorizedStatistics.$F, width, data1, data2)
-        $R(width::Integer, data1, data2, weights) =
-            running(VectorizedStatistics.$F, width, data1, data2, weights, weights)
-        $R(width::Integer, data1, data2, weights1, weights2) =
-            running(VectorizedStatistics.$F, width, data1, data2, weights1, weights2)
+        function $R(width::Integer, data1, data2; padding = zero(eltype(data1)))
+            result = running(VectorizedStatistics.$F, width, data1, data2)
+            if padding != nopadding
+                result[1] = padding
+            end
+            result
+        end
+        function $R(width::Integer, data1, data2, weights; padding = zero(eltype(data1)))
+            result = running(VectorizedStatistics.$F, width, data1, data2, weights, weights)
+            if padding != nopadding
+                result[1] = padding
+            end
+            result
+        end
+        function $R(width::Integer, data1, data2, weights1, weights2) =
+            result = running(VectorizedStatistics.$F, width, data1, data2, weights1, weights2)
+            if padding != nopadding
+                result[1] = padding
+            end
+            result
+        end
     end
 end
-=#
