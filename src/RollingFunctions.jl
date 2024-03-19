b56b7a259a7e4b49b1f3745245adcba10aa86aad
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2024-03-19 17:42:17 -0400

    remove LoopVectorization

--- src/RollingFunctions.jl
+++ src/RollingFunctions.jl
@@ -34,14 +34,6 @@ using StatsBase:  kurtosis, mad, sem, skewness, variation,
 using KahanSummation: sum_kbn
 using AccurateArithmetic: sum_oro
 
-using LoopVectorization
-
-for T in (:Int8, :Int16, :Int32, :Int64, :Int128,
-          :UInt8, :UInt16, :UInt32, :UInt64, :UInt128,
-          :Float16, :Float32, :Float64)
-  @eval LoopVectorization.check_args(x::Union{Missing,$T}) = true
-end
-
 const Sequence = Union{Vec, Tup} where {N, T, Vec<:AbstractVector{T}, Tup<:NTuple{N,T}}
 
 include("support/exceptions.jl")
